7474a1b67a4a28b79c35cba000bf019e82ab27c3
Chris Harvey <chrisharvey2pi@gmail.com>
2019-06-23 13:04:19 -0400

    simplify Char constructor by computing fields

--- src/class/Scanner.class.ts
+++ src/class/Scanner.class.ts
@@ -16,21 +16,30 @@ function findLastIndex<T>(arr: readonly T[], predicate: (it: T, ix: number) => b
  * @see http://parsingintro.sourceforge.net/#contents_item_4.1
  */
 export class Char {
+	/** The actual character string. */
+	readonly cargo: string;
+	/** Zero-based line number of this character (first line is line 0).*/
+	readonly lineIndex: number;
+	/** Zero-based column number of this character (first col is col 0). */
+	readonly colIndex: number;
+
 	/**
 	 * Construct a new Char object.
-	 *
-	 * @param cargo       The actual character.
-	 * @param sourceIndex The index of the character in source text.
-	 * @param lineIndex   The index of the line the character is on.
-	 * @param colIndex    The index of the column the character is on.
+	 * @param sourceText  - The entire source text.
+	 * @param sourceIndex - The index of the character in source text.
 	 */
-	constructor(
-		readonly cargo       : string,
-		readonly sourceIndex : number,
-		readonly lineIndex   : number,
-		readonly colIndex    : number,
-	) {
+	constructor(readonly sourceText: string, readonly sourceIndex: number) {
+		/** Array of characters from source start until current iteration (not including current character). */
+		const prev_chars: readonly string[] = [...this.sourceText].slice(0, this.sourceIndex)
+		this.cargo = this.sourceText[this.sourceIndex]
+		this.lineIndex = prev_chars.filter((c) => c === '\n').length
+		this.colIndex = this.sourceIndex - ((findLastIndex(prev_chars, (c) => c === '\n') || -1) + 1)
 	}
+
+	/**
+	 * Return a row that describes this character in a table.
+	 * @returns a string representation of this character’s data
+	 */
 	toString(): string {
 		const cargo = new Map([
 			['\u0000' , 'NULL (U+0000)'],
@@ -64,26 +73,15 @@ export default class Scanner {
 	static * generate(sourceText: string): Iterator<[Char, Char|null]> {
 		sourceText= sourceText + ENDMARK
 		for (let source_index = 0; source_index < sourceText.length; source_index++) {
-			/** Array of characters from source start until current iteration (not including current character). */
-			const prev_chars: readonly string[] = [...sourceText].slice(0, source_index)
 			/** The current character. */
 			const curr_char = sourceText[source_index]
-			/** Zero-based line number of the current character (first line is line 0). */
-			const line_index: number = prev_chars.filter((c) => c === '\n').length
-			/** Zero-based column number of the current character (first col is col 0). */
-			const col_index: number = source_index - ((findLastIndex(prev_chars, (c) => c === '\n') || -1) + 1)
 
 			/** The lookahead character: the character after the current character. */
 			const lookahead: string|null = (curr_char === ENDMARK) ? null : sourceText[source_index + 1];
 
 			yield [
-				new Char(curr_char, source_index, line_index, col_index),
-				(lookahead === null) ? null : new Char(
-					lookahead,
-					source_index + 1,
-					(curr_char === '\n') ? line_index + 1 : line_index,
-					(curr_char === '\n') ? 0              : col_index + 1,
-				),
+				new Char(sourceText, source_index),
+				(lookahead === null) ? null : new Char(sourceText, source_index + 1),
 			]
 		}
 	}
