af97fea21785c4b9f51714c015c1ad4370ffc58f
Chris Harvey <chrisharvey2pi@gmail.com>
2019-10-30 10:01:10 -0400

    replace TokenType enum with subclasses of Token

--- src/class/Lexer.class.ts
+++ src/class/Lexer.class.ts
@@ -4,28 +4,12 @@ const whitespace: readonly string[] = [' ', '\t', '\n', '\r']
 
 const digits_dec: readonly string[] = `0 1 2 3 4 5 6 7 8 9`.split(' ')
 
+const word_starts: readonly string[] = ``.split(' ')
+const word_chars: readonly string[] = ``.split(' ')
+
 const punctuators1: readonly string[] = `+ - * / ^ ( )`.split(' ')
 const punctuators2: readonly string[] = ``.split(' ')
 const punctuators3: readonly string[] = ``.split(' ')
-
-const keywords: readonly string[] = ``.split(' ')
-const identifier_chars: readonly string[] = ``.split(' ')
-const identifier_starts: readonly string[] = ``.split(' ')
-
-
-/**
- * The different possible types of tokens.
- */
-export enum TokenType {
-	FILEBOUND,
-	WHITESPACE,
-	NUMBER,
-	PUNCTUATOR,
-	KEYWORD,
-	STRING,
-	IDENTIFIER,
-	COMMENT,
-}
 
 
 /**
@@ -37,26 +21,24 @@ export enum TokenType {
  *
  * @see http://parsingintro.sourceforge.net/#contents_item_6.4
  */
-export class Token {
+export abstract class Token {
 	/** All the characters in this Token. */
 	private _cargo: string;
 	/** Zero-based line number of the first character (first line is line 0). */
 	readonly line_index: number;
 	/** Zero-based column number of the first character (first col is col 0). */
 	readonly col_index: number;
-	/** The token type. */
-	type: TokenType|null;
 
 	/**
 	 * Construct a new Token object.
 	 *
-	 * @param startChar  The starting character of this Token.
+	 * @param start_char - the starting character of this Token
+	 * @param tagname    - the name of the type of this Token
 	 */
-	constructor(startChar: Char) {
-		this._cargo     = startChar.cargo
-		this.line_index = startChar.line_index
-		this.col_index  = startChar.col_index
-		this.type       = null
+	constructor(private readonly tagname: string, start_char: Char) {
+		this._cargo     = start_char.cargo
+		this.line_index = start_char.line_index
+		this.col_index  = start_char.col_index
 	}
 
 	/**
@@ -80,8 +62,7 @@ export class Token {
 	 * @returns a string formatted as an XML element
 	 */
 	serialize(): string {
-		if (this.type === null) return ''
-		const tagname: string = TokenType[this.type]
+		const tagname: string = this.tagname
 		const attributes: string = (this.cargo !== STX && this.cargo !== ETX) ? ' ' + [
 			`line="${this.line_index+1}"`,
 			`col="${this.col_index+1}"`,
@@ -93,6 +74,13 @@ export class Token {
 		return `<${tagname}${attributes}>${cargo}</${tagname}>`
 	}
 }
+export class TokenFilebound  extends Token { constructor(start_char: Char) { super('FILEBOUND' , start_char) } }
+export class TokenWhitespace extends Token { constructor(start_char: Char) { super('WHITESPACE', start_char) } }
+export class TokenComment    extends Token { constructor(start_char: Char) { super('COMMENT'   , start_char) } }
+export class TokenString     extends Token { constructor(start_char: Char) { super('STRING'    , start_char) } }
+export class TokenNumber     extends Token { constructor(start_char: Char) { super('NUMBER'    , start_char) } }
+export class TokenWord       extends Token { constructor(start_char: Char) { super('WORD'      , start_char) } }
+export class TokenPunctuator extends Token { constructor(start_char: Char) { super('PUNCTUATOR', start_char) } }
 
 
 /**
@@ -136,8 +124,7 @@ export default class Lexer {
 		}
 		while (!character.done) {
 			if (whitespace.includes(c0)) {
-				const wstoken = new Token(character.value)
-				wstoken.type = TokenType.WHITESPACE
+				const wstoken = new TokenWhitespace(character.value)
 				advance()
 				while (!character.done && whitespace.includes(c0)) {
 					wstoken.add(c0)
@@ -147,32 +134,30 @@ export default class Lexer {
 				continue;
 			}
 
-			const token = new Token(character.value)
+			let token: Token;
 			if (c0 === STX || c0 === ETX) {
-				token.type = TokenType.FILEBOUND
+				token = new TokenFilebound(character.value)
 				advance()
 			// TODO comments
 			} else if (digits_dec.includes(c0)) {
-				token.type = TokenType.NUMBER
+				token = new TokenNumber(character.value)
 				advance()
 				while (!character.done && digits_dec.includes(c0)) {
 					token.add(c0)
 					advance()
 				}
-			} else if (identifier_starts.includes(c0)) {
-				token.type = TokenType.IDENTIFIER
+			} else if (word_starts.includes(c0)) {
+				token = new TokenWord(character.value)
 				advance()
-				while (!character.done && identifier_chars.includes(c0)) {
+				while (!character.done && word_chars.includes(c0)) {
 					token.add(c0)
 					advance()
 				}
-				if (keywords.includes(token.cargo)) {
-					token.type = TokenType.KEYWORD
-				}
 			} else if (punctuators1.includes(c0)) {
-				token.type = TokenType.PUNCTUATOR
+				token = new TokenPunctuator(character.value)
 				let first_char: string = c0
 				advance() // read past the first character
+				// TODO clean this up when we get to multi-char punctuators
 				if (punctuators2.includes(first_char + c0)) {
 					token.add(c0)
 					let second_char: string = c0
