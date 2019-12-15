fcf5e8e21ed27416607c999932cb6d68e92c0853
Chris Harvey <chrisharvey2pi@gmail.com>
2019-12-15 01:28:17 -0500

    use Char param for LexError01

--- src/error/LexError.class.ts
+++ src/error/LexError.class.ts
@@ -1,5 +1,7 @@
-import SolidError from './SolidError.class'
+import {Char} from '../class/Scanner.class'
 import Token from '../class/Token.class'
+
+import SolidError from './SolidError.class'
 
 
 export default class LexError extends SolidError {
@@ -17,19 +19,19 @@ export default class LexError extends SolidError {
 }
 export class LexError01 extends LexError {
 	static readonly CODE = 1
-	constructor (char: string, line: number, col: number) { // TODO use `Char` class
-		super(`Unrecognized character: \`${char}\` at line ${line+1} col ${col+1}.`, LexError01.CODE, line, col);
+	constructor (char: Char) {
+		super(`Unrecognized character: \`${char.source}\` at line ${char.line_index + 1} col ${char.col_index + 1}.`, LexError01.CODE, char.line_index, char.col_index)
 	}
 }
 export class LexError02 extends LexError {
 	static readonly CODE = 2
 	constructor (token: Token) {
-		super(`Found end of file before end of ${token.tagname}.`, LexError02.CODE, token.line_index, token.col_index);
+		super(`Found end of file before end of ${token.tagname}.`, LexError02.CODE, token.line_index, token.col_index)
 	}
 }
 export class LexError03 extends LexError {
 	static readonly CODE = 3
 	constructor (span: string, line: number, col: number) {
-		super(`Invalid escape sequence: \`${span}\` at line ${line+1} col ${col+1}.`, LexError03.CODE, line, col);
+		super(`Invalid escape sequence: \`${span}\` at line ${line + 1} col ${col + 1}.`, LexError03.CODE, line, col)
 	}
 }
