49904b0d0ad7d2e387d6e42a63547d545b20b48d
Chris Harvey <chrisharvey2pi@gmail.com>
2020-01-20 23:02:38 -0500

    self-closing SemanticNode tags

--- test/decorate.test.js
+++ test/decorate.test.js
@@ -15,7 +15,7 @@ test('Decorate file with single token.', () => {
 	const node = new Parser('42').parse()
 	expect(node.decorate().serialize()).toBe(`
 <Goal source=\"␂ 42 ␃\">
-	<Constant line="1" col="1" source="42" value="42"></Constant>
+	<Constant line="1" col="1" source="42" value="42"/>
 </Goal>
 	`.replace(/\n\t*/g, ''))
 	const tree = new Parser('').parse()
@@ -30,7 +30,7 @@ test('Decorate unary symbol.', () => {
 	const node = new Parser('- 42').parse().children[1]
 	expect(node.decorate().serialize()).toBe(`
 <Expression line="1" col="1" source="- 42" operator="-">
-	<Constant line="1" col="3" source="42" value="42"></Constant>
+	<Constant line="1" col="3" source="42" value="42"/>
 </Expression>
 	`.replace(/\n\t*/g, ''))
 })
@@ -41,8 +41,8 @@ test('Decorate exponential.', () => {
 	const node = new Parser('2 ^ -3').parse().children[1]
 	expect(node.decorate().serialize()).toBe(`
 <Expression line="1" col="1" source="2 ^ -3" operator="^">
-	<Constant line="1" col="1" source="2" value="2"></Constant>
-	<Constant line="1" col="5" source="-3" value="-3"></Constant>
+	<Constant line="1" col="1" source="2" value="2"/>
+	<Constant line="1" col="5" source="-3" value="-3"/>
 </Expression>
 	`.replace(/\n\t*/g, ''))
 })
@@ -53,8 +53,8 @@ test('Decorate multiplicative.', () => {
 	const node = new Parser('2 * -3').parse().children[1]
 	expect(node.decorate().serialize()).toBe(`
 <Expression line="1" col="1" source="2 * -3" operator="*">
-	<Constant line="1" col="1" source="2" value="2"></Constant>
-	<Constant line="1" col="5" source="-3" value="-3"></Constant>
+	<Constant line="1" col="1" source="2" value="2"/>
+	<Constant line="1" col="5" source="-3" value="-3"/>
 </Expression>
 	`.replace(/\n\t*/g, ''))
 })
@@ -65,8 +65,8 @@ test('Decorate additive.', () => {
 	const node = new Parser('2 + -3').parse().children[1]
 	expect(node.decorate().serialize()).toBe(`
 <Expression line="1" col="1" source="2 + -3" operator="+">
-	<Constant line="1" col="1" source="2" value="2"></Constant>
-	<Constant line="1" col="5" source="-3" value="-3"></Constant>
+	<Constant line="1" col="1" source="2" value="2"/>
+	<Constant line="1" col="5" source="-3" value="-3"/>
 </Expression>
 	`.replace(/\n\t*/g, ''))
 })
@@ -77,8 +77,8 @@ test('Decorate grouping.', () => {
 	const node = new Parser('(2 + -3)').parse().children[1]
 	expect(node.decorate().serialize()).toBe(`
 <Expression line="1" col="2" source="2 + -3" operator="+">
-	<Constant line="1" col="2" source="2" value="2"></Constant>
-	<Constant line="1" col="6" source="-3" value="-3"></Constant>
+	<Constant line="1" col="2" source="2" value="2"/>
+	<Constant line="1" col="6" source="-3" value="-3"/>
 </Expression>
 	`.replace(/\n\t*/g, ''))
 })
