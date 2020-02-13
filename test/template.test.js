e8ee706184faa4524f3ca899df7a17bf50db3e00
Chris Harvey <chrisharvey2pi@gmail.com>
2020-02-13 17:38:27 -0500

    add test "Parse `StringTemplate` expression units"

--- test/template.test.js
+++ test/template.test.js
@@ -1,5 +1,6 @@
 const {default: Lexer} = require('../build/class/Lexer.class.js')
 const {default: Screener} = require('../build/class/Screener.class.js')
+const {default: Parser} = require('../build/class/Parser.class.js')
 const {
 	TokenTemplate,
 	TemplatePosition,
@@ -175,4 +176,272 @@ test('Screener computes `TokenTemplate` values.', () => {
 	expect(tokens[28].cook()).toBe(`0 \\' 1 \\\\ 2 \\s 3 \\t 4 \\n 5 \\r 6 \\\` 7`)
 	expect(tokens[30].cook()).toBe(`0 \\u{24} 1 \\u{005f} 2 \\u{} 3`)
 	expect(tokens[32].cook()).toBe(`012\\\n345\n678`)
+})
+
+
+
+describe('Parse `StringTemplate` expression units.', () => {
+	const stringTemplateParseNode = (goal) => goal
+		.children[1] // Goal__0__List
+		.children[0] // Statement
+		.children[0] // Expression
+		.children[0] // ExpressionAdditive
+		.children[0] // ExpressionMultiplicative
+		.children[0] // ExpressionExponential
+		.children[0] // ExpressionUnarySymbol
+		.children[0] // ExpressionUnit
+		.children[0] // StringTemplate
+
+	test('Head, tail.', () => {
+		expect(stringTemplateParseNode(new Parser(`
+\`head1{{}}tail1\`;
+		`.trim()).parse()).serialize()).toBe(`
+<StringTemplate line="1" col="1" source="\`head1{{ }}tail1\`">
+	<TEMPLATE line="1" col="1" value="head1">\`head1{{</TEMPLATE>
+	<TEMPLATE line="1" col="9" value="tail1">}}tail1\`</TEMPLATE>
+</StringTemplate>
+		`.replace(/\n\t*/g, ''))
+	})
+
+	test('Head, expr, tail.', () => {
+		expect(stringTemplateParseNode(new Parser(`
+\`head1{{ \`full1\` }}tail1\`;
+		`.trim()).parse()).serialize()).toBe(`
+<StringTemplate line="1" col="1" source="\`head1{{ \`full1\` }}tail1\`">
+	<TEMPLATE line="1" col="1" value="head1">\`head1{{</TEMPLATE>
+	<Expression line="1" col="10" source="\`full1\`">
+		<ExpressionAdditive line="1" col="10" source="\`full1\`">
+			<ExpressionMultiplicative line="1" col="10" source="\`full1\`">
+				<ExpressionExponential line="1" col="10" source="\`full1\`">
+					<ExpressionUnarySymbol line="1" col="10" source="\`full1\`">
+						<ExpressionUnit line="1" col="10" source="\`full1\`">
+							<StringTemplate line="1" col="10" source="\`full1\`">
+								<TEMPLATE line="1" col="10" value="full1">\`full1\`</TEMPLATE>
+							</StringTemplate>
+						</ExpressionUnit>
+					</ExpressionUnarySymbol>
+				</ExpressionExponential>
+			</ExpressionMultiplicative>
+		</ExpressionAdditive>
+	</Expression>
+	<TEMPLATE line="1" col="18" value="tail1">}}tail1\`</TEMPLATE>
+</StringTemplate>
+		`.replace(/\n\t*/g, ''))
+	})
+
+	test('Head, expr, middle, tail.', () => {
+		expect(stringTemplateParseNode(new Parser(`
+\`head1{{ \`full1\` }}midd1{{}}tail1\`;
+		`.trim()).parse()).serialize()).toBe(`
+<StringTemplate line="1" col="1" source="\`head1{{ \`full1\` }}midd1{{ }}tail1\`">
+	<TEMPLATE line="1" col="1" value="head1">\`head1{{</TEMPLATE>
+	<Expression line="1" col="10" source="\`full1\`">
+		<ExpressionAdditive line="1" col="10" source="\`full1\`">
+			<ExpressionMultiplicative line="1" col="10" source="\`full1\`">
+				<ExpressionExponential line="1" col="10" source="\`full1\`">
+					<ExpressionUnarySymbol line="1" col="10" source="\`full1\`">
+						<ExpressionUnit line="1" col="10" source="\`full1\`">
+							<StringTemplate line="1" col="10" source="\`full1\`">
+								<TEMPLATE line="1" col="10" value="full1">\`full1\`</TEMPLATE>
+							</StringTemplate>
+						</ExpressionUnit>
+					</ExpressionUnarySymbol>
+				</ExpressionExponential>
+			</ExpressionMultiplicative>
+		</ExpressionAdditive>
+	</Expression>
+	<StringTemplate__0__List line="1" col="18" source="}}midd1{{">
+		<TEMPLATE line="1" col="18" value="midd1">}}midd1{{</TEMPLATE>
+	</StringTemplate__0__List>
+	<TEMPLATE line="1" col="27" value="tail1">}}tail1\`</TEMPLATE>
+</StringTemplate>
+		`.replace(/\n\t*/g, ''))
+	})
+
+	test('Head, expr, middle, expr, tail.', () => {
+		expect(stringTemplateParseNode(new Parser(`
+\`head1{{ \`full1\` }}midd1{{ \`full2\` }}tail1\`;
+		`.trim()).parse()).serialize()).toBe(`
+<StringTemplate line="1" col="1" source="\`head1{{ \`full1\` }}midd1{{ \`full2\` }}tail1\`">
+	<TEMPLATE line="1" col="1" value="head1">\`head1{{</TEMPLATE>
+	<Expression line="1" col="10" source="\`full1\`">
+		<ExpressionAdditive line="1" col="10" source="\`full1\`">
+			<ExpressionMultiplicative line="1" col="10" source="\`full1\`">
+				<ExpressionExponential line="1" col="10" source="\`full1\`">
+					<ExpressionUnarySymbol line="1" col="10" source="\`full1\`">
+						<ExpressionUnit line="1" col="10" source="\`full1\`">
+							<StringTemplate line="1" col="10" source="\`full1\`">
+								<TEMPLATE line="1" col="10" value="full1">\`full1\`</TEMPLATE>
+							</StringTemplate>
+						</ExpressionUnit>
+					</ExpressionUnarySymbol>
+				</ExpressionExponential>
+			</ExpressionMultiplicative>
+		</ExpressionAdditive>
+	</Expression>
+	<StringTemplate__0__List line="1" col="18" source="}}midd1{{ \`full2\`">
+		<TEMPLATE line="1" col="18" value="midd1">}}midd1{{</TEMPLATE>
+		<Expression line="1" col="28" source="\`full2\`">
+			<ExpressionAdditive line="1" col="28" source="\`full2\`">
+				<ExpressionMultiplicative line="1" col="28" source="\`full2\`">
+					<ExpressionExponential line="1" col="28" source="\`full2\`">
+						<ExpressionUnarySymbol line="1" col="28" source="\`full2\`">
+							<ExpressionUnit line="1" col="28" source="\`full2\`">
+								<StringTemplate line="1" col="28" source="\`full2\`">
+									<TEMPLATE line="1" col="28" value="full2">\`full2\`</TEMPLATE>
+								</StringTemplate>
+							</ExpressionUnit>
+						</ExpressionUnarySymbol>
+					</ExpressionExponential>
+				</ExpressionMultiplicative>
+			</ExpressionAdditive>
+		</Expression>
+	</StringTemplate__0__List>
+	<TEMPLATE line="1" col="36" value="tail1">}}tail1\`</TEMPLATE>
+</StringTemplate>
+		`.replace(/\n\t*/g, ''))
+	})
+
+	test('Head, expr, middle, expr, middle, tail.', () => {
+		expect(stringTemplateParseNode(new Parser(`
+\`head1{{ \`full1\` }}midd1{{ \`full2\` }}midd2{{}}tail1\`;
+		`.trim()).parse()).serialize()).toBe(`
+<StringTemplate line="1" col="1" source="\`head1{{ \`full1\` }}midd1{{ \`full2\` }}midd2{{ }}tail1\`">
+	<TEMPLATE line="1" col="1" value="head1">\`head1{{</TEMPLATE>
+	<Expression line="1" col="10" source="\`full1\`">
+		<ExpressionAdditive line="1" col="10" source="\`full1\`">
+			<ExpressionMultiplicative line="1" col="10" source="\`full1\`">
+				<ExpressionExponential line="1" col="10" source="\`full1\`">
+					<ExpressionUnarySymbol line="1" col="10" source="\`full1\`">
+						<ExpressionUnit line="1" col="10" source="\`full1\`">
+							<StringTemplate line="1" col="10" source="\`full1\`">
+								<TEMPLATE line="1" col="10" value="full1">\`full1\`</TEMPLATE>
+							</StringTemplate>
+						</ExpressionUnit>
+					</ExpressionUnarySymbol>
+				</ExpressionExponential>
+			</ExpressionMultiplicative>
+		</ExpressionAdditive>
+	</Expression>
+	<StringTemplate__0__List line="1" col="18" source="}}midd1{{ \`full2\` }}midd2{{">
+		<StringTemplate__0__List line="1" col="18" source="}}midd1{{ \`full2\`">
+			<TEMPLATE line="1" col="18" value="midd1">}}midd1{{</TEMPLATE>
+			<Expression line="1" col="28" source="\`full2\`">
+				<ExpressionAdditive line="1" col="28" source="\`full2\`">
+					<ExpressionMultiplicative line="1" col="28" source="\`full2\`">
+						<ExpressionExponential line="1" col="28" source="\`full2\`">
+							<ExpressionUnarySymbol line="1" col="28" source="\`full2\`">
+								<ExpressionUnit line="1" col="28" source="\`full2\`">
+									<StringTemplate line="1" col="28" source="\`full2\`">
+										<TEMPLATE line="1" col="28" value="full2">\`full2\`</TEMPLATE>
+									</StringTemplate>
+								</ExpressionUnit>
+							</ExpressionUnarySymbol>
+						</ExpressionExponential>
+					</ExpressionMultiplicative>
+				</ExpressionAdditive>
+			</Expression>
+		</StringTemplate__0__List>
+		<TEMPLATE line="1" col="36" value="midd2">}}midd2{{</TEMPLATE>
+	</StringTemplate__0__List>
+	<TEMPLATE line="1" col="45" value="tail1">}}tail1\`</TEMPLATE>
+</StringTemplate>
+		`.replace(/\n\t*/g, ''))
+	})
+
+	test('Head, expr, middle, expr, middle, expr, tail.', () => {
+		expect(stringTemplateParseNode(new Parser(`
+\`head1{{ \`full1\` }}midd1{{ \`full2\` }}midd2{{ \`head2{{ \`full3\` }}tail2\` }}tail1\`;
+		`.trim()).parse()).serialize()).toBe(`
+<StringTemplate line="1" col="1" source="\`head1{{ \`full1\` }}midd1{{ \`full2\` }}midd2{{ \`head2{{ \`full3\` }}tail2\` }}tail1\`">
+	<TEMPLATE line="1" col="1" value="head1">\`head1{{</TEMPLATE>
+	<Expression line="1" col="10" source="\`full1\`">
+		<ExpressionAdditive line="1" col="10" source="\`full1\`">
+			<ExpressionMultiplicative line="1" col="10" source="\`full1\`">
+				<ExpressionExponential line="1" col="10" source="\`full1\`">
+					<ExpressionUnarySymbol line="1" col="10" source="\`full1\`">
+						<ExpressionUnit line="1" col="10" source="\`full1\`">
+							<StringTemplate line="1" col="10" source="\`full1\`">
+								<TEMPLATE line="1" col="10" value="full1">\`full1\`</TEMPLATE>
+							</StringTemplate>
+						</ExpressionUnit>
+					</ExpressionUnarySymbol>
+				</ExpressionExponential>
+			</ExpressionMultiplicative>
+		</ExpressionAdditive>
+	</Expression>
+	<StringTemplate__0__List line="1" col="18" source="}}midd1{{ \`full2\` }}midd2{{ \`head2{{ \`full3\` }}tail2\`">
+		<StringTemplate__0__List line="1" col="18" source="}}midd1{{ \`full2\`">
+			<TEMPLATE line="1" col="18" value="midd1">}}midd1{{</TEMPLATE>
+			<Expression line="1" col="28" source="\`full2\`">
+				<ExpressionAdditive line="1" col="28" source="\`full2\`">
+					<ExpressionMultiplicative line="1" col="28" source="\`full2\`">
+						<ExpressionExponential line="1" col="28" source="\`full2\`">
+							<ExpressionUnarySymbol line="1" col="28" source="\`full2\`">
+								<ExpressionUnit line="1" col="28" source="\`full2\`">
+									<StringTemplate line="1" col="28" source="\`full2\`">
+										<TEMPLATE line="1" col="28" value="full2">\`full2\`</TEMPLATE>
+									</StringTemplate>
+								</ExpressionUnit>
+							</ExpressionUnarySymbol>
+						</ExpressionExponential>
+					</ExpressionMultiplicative>
+				</ExpressionAdditive>
+			</Expression>
+		</StringTemplate__0__List>
+		<TEMPLATE line="1" col="36" value="midd2">}}midd2{{</TEMPLATE>
+		<Expression line="1" col="46" source="\`head2{{ \`full3\` }}tail2\`">
+			<ExpressionAdditive line="1" col="46" source="\`head2{{ \`full3\` }}tail2\`">
+				<ExpressionMultiplicative line="1" col="46" source="\`head2{{ \`full3\` }}tail2\`">
+					<ExpressionExponential line="1" col="46" source="\`head2{{ \`full3\` }}tail2\`">
+						<ExpressionUnarySymbol line="1" col="46" source="\`head2{{ \`full3\` }}tail2\`">
+							<ExpressionUnit line="1" col="46" source="\`head2{{ \`full3\` }}tail2\`">
+								<StringTemplate line="1" col="46" source="\`head2{{ \`full3\` }}tail2\`">
+									<TEMPLATE line="1" col="46" value="head2">\`head2{{</TEMPLATE>
+									<Expression line="1" col="55" source="\`full3\`">
+										<ExpressionAdditive line="1" col="55" source="\`full3\`">
+											<ExpressionMultiplicative line="1" col="55" source="\`full3\`">
+												<ExpressionExponential line="1" col="55" source="\`full3\`">
+													<ExpressionUnarySymbol line="1" col="55" source="\`full3\`">
+														<ExpressionUnit line="1" col="55" source="\`full3\`">
+															<StringTemplate line="1" col="55" source="\`full3\`">
+																<TEMPLATE line="1" col="55" value="full3">\`full3\`</TEMPLATE>
+															</StringTemplate>
+														</ExpressionUnit>
+													</ExpressionUnarySymbol>
+												</ExpressionExponential>
+											</ExpressionMultiplicative>
+										</ExpressionAdditive>
+									</Expression>
+									<TEMPLATE line="1" col="63" value="tail2">}}tail2\`</TEMPLATE>
+								</StringTemplate>
+							</ExpressionUnit>
+						</ExpressionUnarySymbol>
+					</ExpressionExponential>
+				</ExpressionMultiplicative>
+			</ExpressionAdditive>
+		</Expression>
+	</StringTemplate__0__List>
+	<TEMPLATE line="1" col="72" value="tail1">}}tail1\`</TEMPLATE>
+</StringTemplate>
+		`.replace(/\n\t*/g, ''))
+	})
+
+	test('Orphaned head throws.', () => {
+		expect(() => new Parser(`
+\`A string template head token not followed by a middle or tail {{ 1;
+		`.trim()).parse()).toThrow('Unexpected token')
+	})
+
+	test('Orphaned middle throws.', () => {
+		expect(() => new Parser(`
+2 }} a string template middle token not preceded by a head/middle and not followed by a middle/tail {{ 3;
+		`.trim()).parse()).toThrow('Unexpected token')
+	})
+
+	test('Orphaned tail throws.', () => {
+		expect(() => new Parser(`
+4 }} a string template tail token not preceded by a head or middle\`;
+		`.trim()).parse()).toThrow('Unexpected token')
+	})
 })
