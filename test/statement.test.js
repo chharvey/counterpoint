f0811056a0ea5e0a85515f68d192eff11e1ff6b3
Chris Harvey <chrisharvey2pi@gmail.com>
2020-03-20 19:16:41 -0400

    combine empty statement tests into one block

--- test/statement.test.js
+++ test/statement.test.js
@@ -2,9 +2,10 @@ const {default: Parser} = require('../build/class/Parser.class.js')
 
 
 
-test('Parse empty statement.', () => {
-	const parser = new Parser(';')
-	expect(parser.parse().serialize()).toBe(`
+describe('Empty statements.', () => {
+	test('Parse empty statement.', () => {
+		const parser = new Parser(';')
+		expect(parser.parse().serialize()).toBe(`
 <Goal source="␂ ; ␃">
 	<FILEBOUND value="true">␂</FILEBOUND>
 	<Goal__0__List line="1" col="1" source=";">
@@ -14,30 +15,27 @@ test('Parse empty statement.', () => {
 	</Goal__0__List>
 	<FILEBOUND value="false">␃</FILEBOUND>
 </Goal>
-	`.replace(/\n\t*/g, ''))
-})
+		`.replace(/\n\t*/g, ''))
+	})
 
-
-
-test('Decorate empty statement.', () => {
-	const parser = new Parser(';')
-	expect(parser.parse().decorate().serialize()).toBe(`
+	test('Decorate empty statement.', () => {
+		const parser = new Parser(';')
+		expect(parser.parse().decorate().serialize()).toBe(`
 <Goal source="␂ ; ␃">
 	<StatementList line="1" col="1" source=";">
 		<Statement line="1" col="1" source=";" type="expression"/>
 	</StatementList>
 </Goal>
-	`.replace(/\n\t*/g, ''))
-})
+		`.replace(/\n\t*/g, ''))
+	})
 
-
-
-test('Compile empty statement.', () => {
-	const parser = new Parser(';')
-	expect(parser.parse().decorate().compile()).toBe(`
+	test('Compile empty statement.', () => {
+		const parser = new Parser(';')
+		expect(parser.parse().decorate().compile()).toBe(`
 export default void 0
 export default __2
-	`.trim())
+		`.trim())
+	})
 })
 
 
