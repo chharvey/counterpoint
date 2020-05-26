28a2c087e641f0a0453585405aaf6905cf5bee0b
Chris Harvey <chrisharvey2pi@gmail.com>
2020-05-25 22:18:00 -0400

    convert some options to commands

--- src/solid.ts
+++ src/solid.ts
@@ -10,34 +10,40 @@ import {
 
 
 const helptext: string = `
-	Usage: solid [options] filepath
+	Usage: solid <command> <filepath> [options]
 
-	Parse, analyze, and compile a Solid source code file into an executable binary.
-	Or if the \`--run\` option is provided, run the executable.
-	Executables are in WASM format. See [WebAssembly](https://webassembly.org/) for details.
+	Parse, analyze, and compile a Solid source code file.
+	Executables are in WASM binary format. Plaintext outputs are in WAT format.
+	See [WebAssembly](https://webassembly.org/) for details.
 
 	Examples:
 	\`\`\`
 	# Compile \`test.solid\` to \`test.wasm\`:
-	$ solid test.solid
+	$ solid compile test.solid
 
 	# Compile \`src/input.solid\` to \`build/output.wasm\`:
-	$ solid src/input.solid -o build/output.wasm
+	$ solid compile src/input.solid --out build/output.wasm
+
+	# Debug \`program.solid\` (writes to \`program.wat\`):
+	$ solid dev program.solid
 
 	# Execute \`program.wasm\`:
-	$ solid -r program.wasm
+	$ solid run program.wasm
 	\`\`\`
+
+	Commands:
+	help                     Print this help message.
+	version                  Print the version of Solid currently installed.
+	c, compile               Compile a Solid file into a \`*.wasm\` executable binary.
+	d, dev                   Compile a Solid file into a \`*.wat\` text file for inspection.
+	r, run                   Execute a compiled binary file. Ignore the \`--out\` option.
 
 	Options:
 	-h, --help               Print this help message.
 	-v, --version            Print the version of Solid currently installed.
-	-o, --output=file        Specify the output file.
+	-o, --out=file           Specify the output file.
 	                         Otherwise, the default output filepath is the input filepath except
-	                         with the extension changed to \`.wasm\` (or \`.wat\` if \`-d\` is given).
-	-d, --debug              Save compilation as \`*.wat\` source code for inspection,
-	                         instead of \`*.wasm\` binary.
-	                         If \`-o\` is given, save to that filepath.
-	-r, --run                Execute the input binary file. Ignore all other options.
+	                         with the extension changed to \`.wasm\` (compile) or \`.wat\` (dev).
 `
 const argv = minimist<{
 	/** Display help text. */
@@ -45,67 +51,80 @@ const argv = minimist<{
 	/** Display version number. */
 	version: boolean;
 	/** If compiling, specify output filepath. */
-	output: string;
-	/** Save intermediate representation (*.wat) output file for inspecting. */
-	debug: boolean;
-	/** Run (execute) an already-compiled binary file. */
-	run: boolean;
+	out: string;
 }>(process.argv.slice(2), {
 	boolean: [
 		'help',
 		'version',
-		'debug',
-		'run',
 	],
 	string: [
-		'output',
+		'out',
 	],
 	alias: {
 		h: 'help',
 		v: 'version',
-		o: 'output',
-		d: 'debug',
-		r: 'run',
+		o: 'out',
 	}
 })
 const valid_args: boolean = (
 	typeof argv.help  === 'boolean' &&
 	typeof argv.version === 'boolean' &&
-	typeof argv.debug === 'boolean' &&
-	typeof argv.run   === 'boolean' &&
-	(argv.output === void 0 || typeof argv.output === 'string' && argv.output !== '')
+	(argv.out === void 0 || typeof argv.out === 'string' && argv.out !== '')
 )
 if (!valid_args) throw new Error(`
 	Invalid CLI arguments!
 	${ helptext }
 `)
-if (argv.version) {
+
+
+enum Command {
+	HELP,
+	VERSION,
+	COMPILE,
+	DEV,
+	RUN,
+}
+const command: Command =
+	(argv.help) ? Command.HELP :
+	(argv.version) ? Command.VERSION :
+	new Map<string, Command>([
+		['help'    , Command.HELP],
+		['version' , Command.VERSION],
+		['compile' , Command.COMPILE],
+		['c'       , Command.COMPILE],
+		['dev'     , Command.DEV],
+		['d'       , Command.DEV],
+		['run'     , Command.RUN],
+		['r'       , Command.RUN],
+	]).get(argv._[0]) || Command.HELP
+if (command === Command.VERSION) {
 	console.log(`solid version ${ require('../package.json').version }`)
 	process.exit(0)
-} else if (argv.help || !argv._[0]) {
+} else if (command === Command.HELP) {
 	console.log(helptext)
 	process.exit(0)
 }
 
-
-const inputpath: string = path.normalize(argv._[0])
-const outputpath: string|null = argv.output ? path.normalize(argv.output) : null
-
+if (!argv._[1]) throw new Error(`
+	No path specified!
+	${ helptext }
+`)
+const inputpath: string = path.normalize(argv._[1])
 const inputfilepath: string = path.join(process.cwd(), inputpath)
-const outputfilepath: string = path.join(process.cwd(), outputpath || path.format({
-	...path.parse(inputpath),
-	base: void 0,
-	ext: argv.debug ? '.wat' : '.wasm',
-}))
 
-
-if (!argv.run) {
+if (command === Command.COMPILE || command === Command.DEV) {
+	const outputpath: string|null = argv.out ? path.normalize(argv.out) : null
+	const outputfilepath: string = path.join(process.cwd(), outputpath || path.format({
+		...path.parse(inputpath),
+		base: void 0,
+		ext: command === Command.DEV ? '.wat' : '.wasm',
+	}))
 	const sourcecode: Promise<string> = fs.promises.readFile(inputfilepath, 'utf8')
 
 	console.log(`
 		Compiling………
 		Source file: ${ inputfilepath }
-		${argv.debug
+		${command === Command.DEV
 			? `Intermediate text file (for debugging):`
 			: `Destination binary file:`
 		} ${ outputfilepath }
@@ -113,7 +132,7 @@ if (!argv.run) {
 
 	;(async () => {
 		const generator: CodeGenerator = new CodeGenerator(await sourcecode)
-		await fs.promises.writeFile(outputfilepath, argv.debug ? generator.print() : generator.compile())
+		await fs.promises.writeFile(outputfilepath, command === Command.DEV ? generator.print() : generator.compile())
 		return console.log('Success!')
 	})().catch((err) => {
 		console.error(err)
