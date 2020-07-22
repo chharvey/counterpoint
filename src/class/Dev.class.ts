314afbc5f453afb011bc04607788a8d56553eda6
Chris Harvey <chrisharvey2pi@gmail.com>
2020-07-22 00:01:14 -0400

    0.2.0-alpha.0 && update feature flag check

--- src/class/Dev.class.ts
+++ src/class/Dev.class.ts
@@ -41,23 +41,23 @@ export default class Dev {
 		readonly typingExplicit  : string,
 	} = {
 		// v0.1.0
-		literalNumber   : '>=0.1.0',
-		operatorsMath   : '>=0.1.0',
-		expressions     : '>=0.1.0',
-		constantFolding : '>=0.1.0',
+		literalNumber:   '>=0.1.*',
+		operatorsMath:   '>=0.1.*',
+		expressions:     '>=0.1.*',
+		constantFolding: '>=0.1.*',
 		// v0.2.0
-		comments          : '>=0.2.0',
-		integerRadices    : '>=0.2.0',
-		numericSeparators : '>=0.2.0',
-		keywordPrimitives : '>=0.2.0',
-		operatorsLogic    : '>=0.2.0',
-		typingImplicit    : '>=0.2.0',
-		statements        : '>=0.2.0',
+		comments:          '>=0.2.*',
+		integerRadices:    '>=0.2.*',
+		numericSeparators: '>=0.2.*',
+		keywordPrimitives: '>=0.2.*',
+		operatorsLogic:    '>=0.2.*',
+		typingImplicit:    '>=0.2.*',
+		statements:        '>=0.2.*',
 		// v0.3.0
-		literalString   : '>=0.3.0',
-		literalTemplate : '>=0.3.0',
-		variables       : '>=0.3.0',
-		typingExplicit  : '>=0.3.0',
+		literalString:   '>=0.3.*',
+		literalTemplate: '>=0.3.*',
+		variables:       '>=0.3.*',
+		typingExplicit:  '>=0.3.*',
 	}
 
 	/**
@@ -66,7 +66,7 @@ export default class Dev {
 	 * @return is this project’s version number in the range of the feature?
 	 */
 	static supports(feature: keyof typeof Dev.FEATURES): boolean {
-		return semver.satisfies(Dev.VERSION, Dev.FEATURES[feature])
+		return semver.satisfies(Dev.VERSION, Dev.FEATURES[feature], {includePrerelease: true})
 	}
 	/**
 	 * Returns `true` if this project supports at least one of the given features.
