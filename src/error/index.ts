0ab95f3ce7a1ff76e232c0d14ff6b0039b4e0c31
Chris Harvey <chrisharvey2pi@gmail.com>
2020-12-24 00:11:33 -0500

    rename Error files
    renamed:    src/error/AssignmentError.class.ts -> src/error/AssignmentError.ts
    renamed:    src/error/LexError.class.ts -> src/error/LexError.ts
    renamed:    src/error/NanError.class.ts -> src/error/NanError.ts
    renamed:    src/error/SolidReferenceError.class.ts -> src/error/SolidReferenceError.ts
    renamed:    src/error/SolidTypeError.class.ts -> src/error/SolidTypeError.ts

--- src/error/index.ts
+++ src/error/index.ts
@@ -2,24 +2,24 @@ export {
 	LexError03,
 	LexError04,
 	LexError05,
-} from './LexError.class';
+} from './LexError';
 
 export {
 	ReferenceError01,
 	ReferenceError02,
-} from './SolidReferenceError.class';
+} from './SolidReferenceError';
 
 export {
 	AssignmentError01,
 	AssignmentError10,
-} from './AssignmentError.class';
+} from './AssignmentError';
 
 export {
 	TypeError01,
 	TypeError03,
-} from './SolidTypeError.class';
+} from './SolidTypeError';
 
 export {
 	NanError01,
 	NanError02,
-} from './NanError.class';
+} from './NanError';
