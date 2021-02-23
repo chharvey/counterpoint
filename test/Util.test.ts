1e8cb5e59727bfd5f8848a7c2733f1bdf60462ed
Chris Harvey <chrisharvey2pi@gmail.com>
2021-02-23 02:29:03 -0500

    add UTF-8 overlong validation

--- test/Util.test.ts
+++ test/Util.test.ts
@@ -113,9 +113,14 @@ describe('Util', () => {
 		].forEach((expect, continuations) => {
 			const code_unit_tpl: readonly string[] = code_unit_templates[continuations];
 			specify(code_unit_tpl.join(', '), () => {
-				const encodings: readonly EncodedChar[] = Array.from(new Array(100), () =>
-					code_unit_tpl.map((unit_tpl) => fromTemplate(unit_tpl)) as readonly number[] as EncodedChar
-				);
+				const encodings: readonly EncodedChar[] = Array.from(new Array(100), () => Util.utf8Encode(
+					(code_unit_tpl.length === 1) ? randomInt(         0,        0x80) :
+					(code_unit_tpl.length === 2) ? randomInt(      0x80,       0x800) :
+					(code_unit_tpl.length === 3) ? randomInt(     0x800,    0x1_0000) :
+					(code_unit_tpl.length === 4) ? randomInt(  0x1_0000,   0x20_0000) :
+					(code_unit_tpl.length === 5) ? randomInt( 0x20_0000,  0x400_0000) :
+					                               randomInt(0x400_0000, 0x8000_0000)
+				));
 				assert.deepStrictEqual(
 					encodings.map((codeunits) => Util.utf8Decode(codeunits)),
 					encodings.map((codeunits) => expect(codeunits)),
@@ -166,6 +171,25 @@ describe('Util', () => {
 						assert.ok(err instanceof UTF8DecodeError, 'thrown error was not an instance of UTF8DecodeError');
 						assert.strictEqual(err.message, `Invalid sequence of code points: ${ invalid }`);
 						assert.deepStrictEqual(err.index, sequence.length - 1, `testing code points: ${ invalid }`);
+						return true;
+					});
+				});
+			};
+		});
+		it('throws if a sequence of code units is not valid.', () => {
+			for (let i = 0; i < 10; i++) {
+				([
+					['1100_000b', '10bb_bbbb'],
+					['1110_0000', '100b_bbbb', '10bb_bbbb'],
+					['1111_0000', '1000_bbbb', '10bb_bbbb', '10bb_bbbb'],
+					['1111_1000', '1000_0bbb', '10bb_bbbb', '10bb_bbbb', '10bb_bbbb'],
+					['1111_1100', '1000_000b', '10bb_bbbb', '10bb_bbbb', '10bb_bbbb', '10bb_bbbb'],
+				]).map((sequence) => sequence.map((tpl) => fromTemplate(tpl))).forEach((sequence, i) => {
+					assert.throws(() => Util.utf8Decode(sequence as readonly number[] as EncodedChar), (err) => {
+						const invalid: string = sequence.map((n) => `0x${ n.toString(16) }`).join();
+						assert.ok(err instanceof UTF8DecodeError, 'thrown error was not an instance of UTF8DecodeError');
+						assert.strictEqual(err.message, `Invalid sequence of code points: ${ invalid }`);
+						assert.deepStrictEqual(err.index, (i === 0) ? 0 : 1, `testing code points: ${ invalid }`);
 						return true;
 					});
 				});
