37ca6229eef3871cf84a2eac7483219a56e4d9d8
Chris Harvey <chrisharvey2pi@gmail.com>
2021-03-17 00:49:26 -0400

    formatting & docs

--- src/decorators.ts
+++ src/decorators.ts
@@ -3,18 +3,20 @@
  * When getting a property, check whether it exists in the “database”.
  * If it does, return that value.
  * If it doen’t, compute the value, store it in the database, and then return it.
+ * @typeparam Params      the method’s parameter types
+ * @typeparam Return      the method’s return type
  * @param   _prototype    the prototype that has the method to be decorated
  * @param   _property_key the name of the method to be decorated
  * @param   descriptor    the Property Descriptor of the prototype’s method
  * @returns               `descriptor`, with a new value that is the decorated method
  */
-export function memoizeMethod<Ps extends unknown[], R>(
-	_prototype: object,
+export function memoizeMethod<Params extends unknown[], Return>(
+	_prototype:    object,
 	_property_key: string,
-	descriptor: TypedPropertyDescriptor<(this: object, ...args: Ps) => R>,
+	descriptor:    TypedPropertyDescriptor<(this: object, ...args: Params) => Return>,
 ): typeof descriptor {
 	const method = descriptor.value!;
-	const memomap: WeakMap<object, R> = new WeakMap();
+	const memomap: WeakMap<object, Return> = new WeakMap();
 	descriptor.value = function (...args) {
 		memomap.has(this) || memomap.set(this, method.call(this, ...args));
 		return memomap.get(this)!;
@@ -26,18 +28,19 @@ export function memoizeMethod<Ps extends unknown[], R>(
 
 /**
  * Like {@link memoizeMethod} but for getters.
+ * @typeparam Return      the getter’s return type
  * @param   _prototype    the prototype that has the getter to be decorated
  * @param   _property_key the name of the getter to be decorated
  * @param   descriptor    the Property Descriptor of the prototype’s getter
  * @returns               `descriptor`, with a new `get` that is the decorated getter
  */
-export function memoizeGetter<R>(
-	_prototype: object,
+export function memoizeGetter<Return>(
+	_prototype:    object,
 	_property_key: string,
-	descriptor: TypedPropertyDescriptor<R>,
+	descriptor:    TypedPropertyDescriptor<Return>,
 ): typeof descriptor {
 	const method = descriptor.get!;
-	const memomap: WeakMap<object, R> = new WeakMap();
+	const memomap: WeakMap<object, Return> = new WeakMap();
 	descriptor.get = function () {
 		memomap.has(this) || memomap.set(this, method.call(this));
 		return memomap.get(this)!;
@@ -51,15 +54,16 @@ export function memoizeGetter<R>(
  * Decorator for run-once methods.
  * The first time the method is called, it should execute; any time after that, it should not.
  * Should only be used on methods that return `void`; for non-void methods, use {@link memoizeMethod}.
+ * @typeparam Params      the method’s parameter types
  * @param   _prototype    the prototype that has the method to be decorated
  * @param   _property_key the name of the method to be decorated
  * @param   descriptor    the Property Descriptor of the prototype’s method
  * @returns               `descriptor`, with a new value that is the decorated method
  */
-export function runOnceMethod<Ps extends unknown[]>(
-	_prototype: object,
+export function runOnceMethod<Params extends unknown[]>(
+	_prototype:    object,
 	_property_key: string,
-	descriptor: TypedPropertyDescriptor<(this: object, ...args: Ps) => void>,
+	descriptor:    TypedPropertyDescriptor<(this: object, ...args: Params) => void>,
 ): typeof descriptor {
 	const method = descriptor.value!;
 	let was_run: boolean = false;
@@ -76,15 +80,16 @@ export function runOnceMethod<Ps extends unknown[]>(
 
 /**
  * Like {@link runOnceMethod} but for setters.
+ * @typeparam Param       the setter’s parameter type
  * @param   _prototype    the prototype that has the getter to be decorated
  * @param   _property_key the name of the getter to be decorated
  * @param   descriptor    the Property Descriptor of the prototype’s getter
  * @returns               `descriptor`, with a new `get` that is the decorated getter
  */
-export function runOnceSetter<P>(
-	_prototype: object,
+export function runOnceSetter<Param>(
+	_prototype:    object,
 	_property_key: string,
-	descriptor: TypedPropertyDescriptor<P>,
+	descriptor:    TypedPropertyDescriptor<Param>,
 ): typeof descriptor {
 	const method = descriptor.set!;
 	let was_run: boolean = false;
@@ -102,15 +107,17 @@ export function runOnceSetter<P>(
 /**
  * Decorator for performing strict equality (`===`), and then disjuncting (`||`) that result
  * with the results of performing the method.
+ * @typeparam Proto       the type of the prototype
+ * @typeparam Params      the method’s parameter types
  * @param   _prototype    the prototype that has the method to be decorated
  * @param   _property_key the name of the method to be decorated
  * @param   descriptor    the Property Descriptor of the prototype’s method
  * @returns               `descriptor`, with a new value that is the decorated method
  */
-export function strictEqual<Proto extends object, Ps extends unknown[]>(
-	_prototype: Proto,
+export function strictEqual<Proto extends object, Params extends unknown[]>(
+	_prototype:    Proto,
 	_property_key: string,
-	descriptor: TypedPropertyDescriptor<(this: Proto, that: Proto, ...args: Ps) => boolean>,
+	descriptor:    TypedPropertyDescriptor<(this: Proto, that: Proto, ...args: Params) => boolean>,
 ): typeof descriptor {
 	const method = descriptor.value!;
 	descriptor.value = function (that, ...args) {
