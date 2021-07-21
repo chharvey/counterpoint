685354f2df01e189425d044476868581d7d7883b
Chris Harvey <chrisharvey2pi@gmail.com>
2021-07-21 10:52:46 -0400

    remove combination algorithm from intersection & union

--- src/typer/SolidTypeTuple.ts
+++ src/typer/SolidTypeTuple.ts
@@ -28,40 +28,6 @@ export class SolidTypeTuple extends SolidType {
 		return v instanceof SolidTuple && v.toType().isSubtypeOf(this);
 	}
 
-	/**
-	 * The *intersection* of types `S` and `T` is the *union* of the set of items on `S` with the set of items on `T`.
-	 * For any overlapping items, their type intersection is taken.
-	 */
-	override intersect_do(t: SolidType): SolidType {
-		if (t instanceof SolidTypeTuple) {
-			const items: SolidType[] = [...this.types];
-			[...t.types].forEach((typ, i) => {
-				items[i] = typ.intersect(this.types[i] || SolidType.UNKNOWN);
-			});
-			return new SolidTypeTuple(items);
-		} else {
-			return super.intersect_do(t);
-		}
-	}
-
-	/**
-	 * The *union* of types `S` and `T` is the *intersection* of the set of items on `S` with the set of items on `T`.
-	 * For any overlapping items, their type union is taken.
-	 */
-	override union_do(t: SolidType): SolidType {
-		if (t instanceof SolidTypeTuple) {
-			const items: SolidType[] = [];
-			[...t.types].forEach((typ, i) => {
-				if (this.types[i]) {
-					items[i] = typ.union(this.types[i]);
-				}
-			})
-			return new SolidTypeTuple(items);
-		} else {
-			return super.union_do(t);
-		}
-	}
-
 	override isSubtypeOf_do(t: SolidType): boolean {
 		return t.equals(SolidObject) || (
 			t instanceof SolidTypeTuple
