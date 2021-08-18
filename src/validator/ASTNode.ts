535ca1c713ad1a92edc1a74e9421e0d96c52e2bb
Chris Harvey <chrisharvey2pi@gmail.com>
2021-08-18 03:01:31 -0400

    ASTNodeDeclarationVariable

--- src/validator/ASTNode.ts
+++ src/validator/ASTNode.ts
@@ -11,7 +11,6 @@ import {
 } from '../parser/index.js';
 import {
 	SolidType,
-	SolidObject,
 	Int16,
 	Float64,
 } from '../typer/index.js';
@@ -20,19 +19,17 @@ import {
 	INST,
 } from '../builder/index.js';
 import {
-	AssignmentError01,
 	AssignmentError10,
 	TypeError03,
 } from '../error/index.js';
 import type {Buildable} from './Buildable.js';
 import {ASTNodeSolid} from './ASTNodeSolid.js';
-import type {ASTNodeType} from './ASTNodeType.js';
 import type {ASTNodeExpression} from './ASTNodeExpression.js';
 import type {ASTNodeVariable} from './ASTNodeVariable.js';
 import {ASTNodeStatement} from './ASTNodeStatement.js';
 import {Decorator} from './Decorator.js';
 import type {Validator} from './Validator.js';
-import {
+import type {
 	SymbolStructureVar,
 } from './SymbolStructure.js';
 
@@ -74,66 +71,10 @@ export * from './ASTNodeOperationTernary.js';
 export * from './ASTNodeStatement.js';
 export * from './ASTNodeDeclaration.js';
 export * from './ASTNodeDeclarationType.js';
+export * from './ASTNodeDeclarationVariable.js';
 
 
 
-export class ASTNodeDeclarationVariable extends ASTNodeStatement {
-	static override fromSource(src: string, config: SolidConfig = CONFIG_DEFAULT): ASTNodeDeclarationVariable {
-		const statement: ASTNodeStatement = ASTNodeStatement.fromSource(src, config);
-		assert.ok(statement instanceof ASTNodeDeclarationVariable);
-		return statement;
-	}
-	constructor (
-		start_node: ParseNode,
-		readonly unfixed: boolean,
-		readonly variable: ASTNodeVariable,
-		readonly type:     ASTNodeType,
-		readonly value:    ASTNodeExpression,
-	) {
-		super(start_node, {unfixed}, [variable, type, value]);
-	}
-	override varCheck(validator: Validator): void {
-		const variable: ASTNodeVariable = this.variable;
-		if (validator.hasSymbol(variable.id)) {
-			throw new AssignmentError01(variable);
-		};
-		this.type.varCheck(validator);
-		this.value.varCheck(validator);
-		validator.addSymbol(new SymbolStructureVar(
-			variable.id,
-			variable.line_index,
-			variable.col_index,
-			variable.source,
-			this.unfixed,
-			() => this.type.assess(validator),
-			(validator.config.compilerOptions.constantFolding && !this.unfixed)
-				? () => this.value.assess(validator)
-				: null,
-		));
-	}
-	override typeCheck(validator: Validator): void {
-		this.type.typeCheck(validator);
-		this.value.typeCheck(validator);
-		const assignee_type: SolidType = this.type.assess(validator);
-		const assigned_type: SolidType = this.value.type(validator);
-		if (
-			assigned_type.isSubtypeOf(assignee_type) ||
-			validator.config.compilerOptions.intCoercion && assigned_type.isSubtypeOf(Int16) && Float64.isSubtypeOf(assignee_type)
-		) {
-		} else {
-			throw new TypeError03(this, assignee_type, assigned_type)
-		}
-		return validator.getSymbolInfo(this.variable.id)?.assess();
-	}
-	override build(builder: Builder): INST.InstructionNone | INST.InstructionDeclareGlobal {
-		const tofloat: boolean = this.type.assess(builder.validator).isSubtypeOf(Float64) || this.value.shouldFloat(builder.validator);
-		const assess: SolidObject | null = this.variable.assess(builder.validator);
-		return (builder.validator.config.compilerOptions.constantFolding && !this.unfixed && assess)
-			? new INST.InstructionNone()
-			: new INST.InstructionDeclareGlobal(this.variable.id, this.unfixed, this.value.build(builder, tofloat))
-		;
-	}
-}
 export class ASTNodeAssignment extends ASTNodeStatement {
 	static override fromSource(src: string, config: SolidConfig = CONFIG_DEFAULT): ASTNodeAssignment {
 		const statement: ASTNodeStatement = ASTNodeStatement.fromSource(src, config);
--- src/validator/index.ts
+++ src/validator/index.ts
@@ -47,6 +47,7 @@ export * from './ASTNodeOperationTernary.js';
 export * from './ASTNodeStatement.js';
 export * from './ASTNodeDeclaration.js';
 export * from './ASTNodeDeclarationType.js';
+export * from './ASTNodeDeclarationVariable.js';
 export * from './ASTNode.js';
 export * as AST from './ASTNode.js';
 
