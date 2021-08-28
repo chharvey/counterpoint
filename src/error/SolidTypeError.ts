595c2927b311e8b5617376606c8481d7f94b1131
Chris Harvey <chrisharvey2pi@gmail.com>
2021-08-28 15:04:18 -0400

    TypeError03

--- src/error/SolidTypeError.ts
+++ src/error/SolidTypeError.ts
@@ -36,26 +36,6 @@ export class SolidTypeError extends ErrorCode {
 
 
 /**
- * A TypeError03 is thrown when an expression is assigned to a type to which it is not assignable,
- * or when an argument is assigned to a parameter to which it is not assignable.
- * @example
- * let x: int = true;             % TypeError03: Expression of type `true` is not assignable to type `int`.
- * ((x: int): int => x + 1)(4.2); % TypeError03: Expression of type `4.2` is not assignable to type `int`.
- */
-export class TypeError03 extends SolidTypeError {
-	/** The number series of this class of errors. */
-	static override readonly CODE = 3;
-	/**
-	 * Construct a new TypeError03 object.
-	 * @param assignment    - the node where the assignment took place
-	 * @param assignee_type - the type to which the expression is assigned
-	 * @param assigned_type - the type of the expression
-	 */
-	constructor (assignment: AST.ASTNodeStatement, assignee_type: SolidType, assigned_type: SolidType) {
-		super(`Expression of type ${ assigned_type } is not assignable to type ${ assignee_type }.`, TypeError03.CODE, assignment.line_index, assignment.col_index)
-	}
-}
-/**
  * A TypeError04 is thrown when an attempt is made to access a non-existent index or property,
  * or when a named argument does not match a known parameter name.
  * @example
