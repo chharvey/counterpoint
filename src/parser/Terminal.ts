ae07f6c660df4b0745af7e78825c5147fc4d1437
Chris Harvey <chrisharvey2pi@gmail.com>
2021-08-28 00:23:01 -0400

    TerminalFloat

--- src/parser/Terminal.ts
+++ src/parser/Terminal.ts
@@ -22,27 +22,6 @@ export * from './terminal/index.js';
 
 
 
-export class TerminalFloat extends Terminal {
-	static readonly instance: TerminalFloat = new TerminalFloat()
-	random(): string {
-		return [
-			maybe(() => Util.arrayRandom(TOKEN.TokenNumber.UNARY)),
-			TerminalInteger.digitSequence(),
-			TOKEN.TokenNumber.POINT,
-			...maybeA(() => [
-				TerminalInteger.digitSequence(),
-				...maybeA(() => [
-					TOKEN.TokenNumber.EXPONENT,
-					maybe(() => Util.arrayRandom(TOKEN.TokenNumber.UNARY)),
-					TerminalInteger.digitSequence(),
-				]),
-			]),
-		].join('')
-	}
-	match(candidate: Token): boolean {
-		return candidate instanceof TOKEN.TokenNumber && candidate.isFloat
-	}
-}
 export class TerminalString extends Terminal {
 	static readonly instance: TerminalString = new TerminalString()
 	private static readonly escape_opts: readonly (() => string)[] = [
--- src/parser/terminal/index.ts
+++ src/parser/terminal/index.ts
@@ -1,3 +1,4 @@
 export * from './TerminalKeyword.js';
 export * from './TerminalIdentifier.js';
 export * from './TerminalInteger.js';
+export * from './TerminalFloat.js';
