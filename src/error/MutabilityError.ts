425c11d360e91cf22489531566edaf39f7c28d45
Chris Harvey <chrisharvey2pi@gmail.com>
2021-09-19 11:41:43 -0400

    update MutabilityError docs & signature

--- src/error/MutabilityError.ts
+++ src/error/MutabilityError.ts
@@ -4,9 +4,21 @@ import {
 
 
 
+/**
+ * A MutabilityError is thrown when the validator recognizes an attempt to mutate an immutable object.
+ */
 export class MutabilityError extends ErrorCode {
+	/** The name of this class of errors. */
 	static override readonly NAME: string = 'MutabilityError';
+	/** The number series of this class of errors. */
 	static readonly CODE: number = 2400;
+	/**
+	 * Construct a new MutabilityError object.
+	 * @param message a message to the user
+	 * @param code    the error number
+	 * @param line    the line index in source code
+	 * @param col     the column index in source code
+	 */
 	constructor (message: string, code: number = 0, line?: number, col?: number) {
 		super({
 			message,
--- src/error/MutabilityError01.ts
+++ src/error/MutabilityError01.ts
@@ -1,14 +1,26 @@
 import type {
-	ASTNode,
-} from '@chharvey/parser';
-import type {SolidType} from './package.js';
+	AST,
+	SolidType,
+} from './package.js';
 import {MutabilityError} from './MutabilityError.js';
 
 
 
+/**
+ * A MutabilityError01 is thrown when the an item or property of an immutable object is reassigned.
+ * @example
+ * let x: [a: int] = [a= 42];
+ * x.a = 43; % MutabilityError01: Mutation of an object of immutable type `[a: int]`.
+ */
 export class MutabilityError01 extends MutabilityError {
+	/** The number series of this class of errors. */
 	static override readonly CODE = 1;
-	constructor (node: ASTNode, typ: SolidType) {
+	/**
+	 * Construct a new MutabilityError01 object.
+	 * @param typ  the type that is being mutated
+	 * @param node the reassignment node where it happens
+	 */
+	constructor (typ: SolidType, node: AST.ASTNodeAssignment) {
 		super(`Mutation of an object of immutable type \`${ typ }\`.`, MutabilityError01.CODE, node.line_index, node.col_index);
 	}
 }
