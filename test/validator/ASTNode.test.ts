758450541a139cc9dc659ba5d1396193d28b418c
Chris Harvey <chrisharvey2pi@gmail.com>
2021-09-19 23:22:19 -0400

    new file:   test/validator/ASTNodeCall.test.ts

--- test/validator/ASTNode.test.ts
+++ test/validator/ASTNode.test.ts
@@ -32,8 +32,6 @@ import {
 	SolidString,
 	SolidTuple,
 	SolidRecord,
-	SolidList,
-	SolidHash,
 	SolidSet,
 	SolidMap,
 } from '../../src/typer/index.js';
@@ -1062,94 +1060,6 @@ describe('ASTNodeSolid', () => {
 				});
 			});
 
-			describe('ASTNodeCall', () => {
-				const validator: Validator = new Validator(CONFIG_FOLDING_OFF);
-				it('evaluates List, Hash, Set, and Map.', () => {
-					assert.deepStrictEqual(
-						[
-							`List.<int>([1, 2, 3]);`,
-							`Hash.<int>([a= 1, b= 2, c= 3]);`,
-							`Set.<int>([1, 2, 3]);`,
-							`Map.<int, float>([
-								[1, 0.1],
-								[2, 0.2],
-								[3, 0.3],
-							]);`,
-						].map((src) => AST.ASTNodeCall.fromSource(src).type(validator)),
-						[
-							new SolidTypeList(Int16),
-							new SolidTypeHash(Int16),
-							new SolidTypeSet(Int16),
-							new SolidTypeMap(Int16, Float64),
-						],
-					);
-				});
-				it('zero/empty functional arguments.', () => {
-					assert.deepStrictEqual(
-						[
-							`List.<int>();`,
-							`Hash.<int>();`,
-							`Set.<int>();`,
-							`Map.<int, float>();`,
-							`List.<int>([]);`,
-							`Set.<int>([]);`,
-							`Map.<int, float>([]);`,
-						].map((src) => AST.ASTNodeCall.fromSource(src).type(validator)),
-						[
-							new SolidTypeList(Int16),
-							new SolidTypeHash(Int16),
-							new SolidTypeSet(Int16),
-							new SolidTypeMap(Int16, Float64),
-							new SolidTypeList(Int16),
-							new SolidTypeSet(Int16),
-							new SolidTypeMap(Int16, Float64),
-						],
-					);
-				});
-				it('Map has a default type parameter.', () => {
-					assert.deepStrictEqual(
-						AST.ASTNodeCall.fromSource(`Map.<int>();`).type(validator),
-						new SolidTypeMap(Int16, Int16),
-					);
-				});
-				it('throws if base is not an ASTNodeVariable.', () => {
-					[
-						`null.();`,
-						`(42 || 43).<bool>();`,
-					].forEach((src) => {
-						assert.throws(() => AST.ASTNodeCall.fromSource(src).type(validator), TypeError05);
-					});
-				});
-				it('throws if base is not one of the allowed strings.', () => {
-					[
-						`SET.<str>();`,
-						`Mapping.<bool>();`,
-					].forEach((src) => {
-						assert.throws(() => AST.ASTNodeCall.fromSource(src).type(validator), SyntaxError);
-					});
-				});
-				it('throws when providing incorrect number of arguments.', () => {
-					[
-						`List.<int>([], []);`,
-						`Hash.<int>([], []);`,
-						`Set.<int>([], []);`,
-						`Map.<int>([], []);`,
-					].forEach((src) => {
-						assert.throws(() => AST.ASTNodeCall.fromSource(src).type(validator), TypeError06);
-					});
-				});
-				it('throws when providing incorrect type of arguments.', () => {
-					[
-						`List.<int>(42);`,
-						`Hash.<int>([4.2]);`,
-						`Set.<int>([42, '42']);`,
-						`Map.<int>([42, '42']);`,
-					].forEach((src) => {
-						assert.throws(() => AST.ASTNodeCall.fromSource(src).type(validator), TypeError03);
-					});
-				});
-			});
-
 			describe('ASTNodeOperation', () => {
 				function typeOperations(tests: ReadonlyMap<string, SolidObject>, config: SolidConfig = CONFIG_DEFAULT): void {
 					return assert.deepStrictEqual(
@@ -1719,68 +1629,6 @@ describe('ASTNodeSolid', () => {
 							[0x101n, new Float64(2.0)],
 							[0x100n, new SolidString('three')],
 						])),
-					);
-				});
-			});
-
-			describe('ASTNodeCall', () => {
-				const validator: Validator = new Validator();
-				it('evaluates List, Hash, Set, and Map.', () => {
-					assert.deepStrictEqual(
-						[
-							`List.<int>([1, 2, 3]);`,
-							`Hash.<int>([a= 1, b= 2, c= 3]);`,
-							`Set.<int>([1, 2, 3]);`,
-							`Map.<int, float>([
-								[1, 0.1],
-								[2, 0.2],
-								[3, 0.4],
-							]);`,
-						].map((src) => AST.ASTNodeCall.fromSource(src).fold(validator)),
-						[
-							new SolidList<Int16>([
-								new Int16(1n),
-								new Int16(2n),
-								new Int16(3n),
-							]),
-							new SolidHash<Int16>(new Map<bigint, Int16>([
-								[0x101n, new Int16(1n)], // 0x100n is "Hash"
-								[0x102n, new Int16(2n)],
-								[0x103n, new Int16(3n)],
-							])),
-							new SolidSet<Int16>(new Set<Int16>([
-								new Int16(1n),
-								new Int16(2n),
-								new Int16(3n),
-							])),
-							new SolidMap<Int16, Float64>(new Map<Int16, Float64>([
-								[new Int16(1n), new Float64(0.1)],
-								[new Int16(2n), new Float64(0.2)],
-								[new Int16(3n), new Float64(0.4)],
-							])),
-						],
-					);
-				});
-				it('zero/empty functional arguments.', () => {
-					assert.deepStrictEqual(
-						[
-							`List.<int>();`,
-							`Hash.<int>();`,
-							`Set.<int>();`,
-							`Map.<int, float>();`,
-							`List.<int>([]);`,
-							`Set.<int>([]);`,
-							`Map.<int, float>([]);`,
-						].map((src) => AST.ASTNodeCall.fromSource(src).fold(validator)),
-						[
-							new SolidList<never>(),
-							new SolidHash<never>(),
-							new SolidSet<never>(),
-							new SolidMap<never, never>(),
-							new SolidList<never>(),
-							new SolidSet<never>(),
-							new SolidMap<never, never>(),
-						],
 					);
 				});
 			});
