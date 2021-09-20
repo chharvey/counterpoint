0ede22dcbf97b0480a4f9acbfb05c0acfa01648d
Chris Harvey <chrisharvey2pi@gmail.com>
2021-09-20 00:46:50 -0400

    new file:   test/validator/ASTNodeOperationBinaryLogical.test.ts

--- test/validator/ASTNode.test.ts
+++ test/validator/ASTNode.test.ts
@@ -53,12 +53,10 @@ import {
 } from '../../src/error/index.js';
 import {
 	assert_wasCalled,
-	assertEqualTypes,
 	assertAssignable,
 } from '../assert-helpers.js';
 import {
 	CONFIG_FOLDING_OFF,
-	TYPE_CONST_NULL,
 	typeConstInt,
 	typeConstFloat,
 	typeConstStr,
@@ -477,70 +475,6 @@ describe('ASTNodeSolid', () => {
 					[...tests.values()],
 				);
 			}
-			describe('ASTNodeOperationBinaryLogical', () => {
-				it('returns InstructionBinopLogical.', () => {
-					assert.deepStrictEqual([
-						`42 && 420;`,
-						`4.2 || -420;`,
-						`null && 201.0e-1;`,
-						`true && 201.0e-1;`,
-						`false || null;`,
-					].map((src) => AST.ASTNodeOperationBinaryLogical.fromSource(src, CONFIG_FOLDING_OFF).build(new Builder(src, CONFIG_FOLDING_OFF))), [
-						new INST.InstructionBinopLogical(
-							0n,
-							Operator.AND,
-							instructionConstInt(42n),
-							instructionConstInt(420n),
-						),
-						new INST.InstructionBinopLogical(
-							0n,
-							Operator.OR,
-							instructionConstFloat(4.2),
-							instructionConstFloat(-420.0),
-						),
-						new INST.InstructionBinopLogical(
-							0n,
-							Operator.AND,
-							instructionConstFloat(0.0),
-							instructionConstFloat(20.1),
-						),
-						new INST.InstructionBinopLogical(
-							0n,
-							Operator.AND,
-							instructionConstFloat(1.0),
-							instructionConstFloat(20.1),
-						),
-						new INST.InstructionBinopLogical(
-							0n,
-							Operator.OR,
-							instructionConstInt(0n),
-							instructionConstInt(0n),
-						),
-					]);
-				});
-				it('counts internal variables correctly.', () => {
-					const src: string = `1 && 2 || 3 && 4;`
-					assert.deepStrictEqual(
-						AST.ASTNodeOperationBinaryLogical.fromSource(src, CONFIG_FOLDING_OFF).build(new Builder(src, CONFIG_FOLDING_OFF)),
-						new INST.InstructionBinopLogical(
-							0n,
-							Operator.OR,
-							new INST.InstructionBinopLogical(
-								1n,
-								Operator.AND,
-								instructionConstInt(1n),
-								instructionConstInt(2n),
-							),
-							new INST.InstructionBinopLogical(
-								2n,
-								Operator.AND,
-								instructionConstInt(3n),
-								instructionConstInt(4n),
-							),
-						),
-					);
-				});
-			});
 			specify('ASTNodeOperationTernary', () => {
 				buildOperations(xjs.Map.mapValues(new Map([
 					[`if true  then false else 2;`,    [new Int16(1n), new Int16(0n),    new Int16(2n)]],
@@ -944,144 +878,6 @@ describe('ASTNodeSolid', () => {
 						[...tests.values()].map((result) => new SolidTypeConstant(result)),
 					);
 				}
-				describe('ASTNodeOperationBinaryLogical', () => {
-					it('with constant folding on.', () => {
-						typeOperations(new Map<string, SolidObject>([
-							[`null  && false;`, SolidNull.NULL],
-							[`false && null;`,  SolidBoolean.FALSE],
-							[`true  && null;`,  SolidNull.NULL],
-							[`false && 42;`,    SolidBoolean.FALSE],
-							[`4.2   && true;`,  SolidBoolean.TRUE],
-							[`null  || false;`, SolidBoolean.FALSE],
-							[`false || null;`,  SolidNull.NULL],
-							[`true  || null;`,  SolidBoolean.TRUE],
-							[`false || 42;`,    new Int16(42n)],
-							[`4.2   || true;`,  new Float64(4.2)],
-						]));
-					});
-					context('with constant folding off.', () => {
-						describe('[operator=AND]', () => {
-							it('returns `left` if it’s a subtype of `void | null | false`.', () => {
-								const goal: AST.ASTNodeGoal = AST.ASTNodeGoal.fromSource(`
-									let unfixed a: null = null;
-									let unfixed b: null | false = null;
-									let unfixed c: null | void = null;
-									a && 42;
-									b && 42;
-									c && 42;
-								`, CONFIG_FOLDING_OFF);
-								const validator: Validator = new Validator(CONFIG_FOLDING_OFF);
-								goal.varCheck(validator);
-								goal.typeCheck(validator);
-								assert.deepStrictEqual(goal.children.slice(3).map((stmt) => (stmt as AST.ASTNodeStatementExpression).expr!.type(validator)), [
-									SolidNull,
-									SolidNull.union(SolidBoolean.FALSETYPE),
-									SolidNull.union(SolidType.VOID),
-								]);
-							});
-							it('returns `T | right` if left is a supertype of `T narrows void | null | false`.', () => {
-								const hello: SolidTypeConstant = typeConstStr('hello');
-								const goal: AST.ASTNodeGoal = AST.ASTNodeGoal.fromSource(`
-									let unfixed a: null | int = null;
-									let unfixed b: null | int = 42;
-									let unfixed c: bool = false;
-									let unfixed d: bool | float = 4.2;
-									let unfixed e: str | void = 'hello';
-									a && 'hello';
-									b && 'hello';
-									c && 'hello';
-									d && 'hello';
-									e && 42;
-								`, CONFIG_FOLDING_OFF);
-								const validator: Validator = new Validator(CONFIG_FOLDING_OFF);
-								goal.varCheck(validator);
-								goal.typeCheck(validator);
-								assert.deepStrictEqual(goal.children.slice(5).map((stmt) => (stmt as AST.ASTNodeStatementExpression).expr!.type(validator)), [
-									SolidNull.union(hello),
-									SolidNull.union(hello),
-									SolidBoolean.FALSETYPE.union(hello),
-									SolidBoolean.FALSETYPE.union(hello),
-									SolidType.VOID.union(typeConstInt(42n)),
-								]);
-							});
-							it('returns `right` if left does not contain `void` nor `null` nor `false`.', () => {
-								const goal: AST.ASTNodeGoal = AST.ASTNodeGoal.fromSource(`
-									let unfixed a: int = 42;
-									let unfixed b: float = 4.2;
-									a && true;
-									b && null;
-								`, CONFIG_FOLDING_OFF);
-								const validator: Validator = new Validator(CONFIG_FOLDING_OFF);
-								goal.varCheck(validator);
-								goal.typeCheck(validator);
-								assert.deepStrictEqual(goal.children.slice(2).map((stmt) => (stmt as AST.ASTNodeStatementExpression).expr!.type(validator)), [
-									SolidBoolean.TRUETYPE,
-									TYPE_CONST_NULL,
-								]);
-							});
-						});
-						describe('[operator=OR]', () => {
-							it('returns `right` if it’s a subtype of `void | null | false`.', () => {
-								const goal: AST.ASTNodeGoal = AST.ASTNodeGoal.fromSource(`
-									let unfixed a: null = null;
-									let unfixed b: null | false = null;
-									let unfixed c: null | void = null;
-									a || false;
-									b || 42;
-									c || 4.2;
-								`, CONFIG_FOLDING_OFF);
-								const validator: Validator = new Validator(CONFIG_FOLDING_OFF);
-								goal.varCheck(validator);
-								goal.typeCheck(validator);
-								assert.deepStrictEqual(goal.children.slice(3).map((stmt) => (stmt as AST.ASTNodeStatementExpression).expr!.type(validator)), [
-									SolidBoolean.FALSETYPE,
-									typeConstInt(42n),
-									typeConstFloat(4.2),
-								]);
-							});
-							it('returns `(left - T) | right` if left is a supertype of `T narrows void | null | false`.', () => {
-								const hello: SolidTypeConstant = typeConstStr('hello');
-								const goal: AST.ASTNodeGoal = AST.ASTNodeGoal.fromSource(`
-									let unfixed a: null | int = null;
-									let unfixed b: null | int = 42;
-									let unfixed c: bool = false;
-									let unfixed d: bool | float = 4.2;
-									let unfixed e: str | void = 'hello';
-									a || 'hello';
-									b || 'hello';
-									c || 'hello';
-									d || 'hello';
-									e || 42;
-								`, CONFIG_FOLDING_OFF);
-								const validator: Validator = new Validator(CONFIG_FOLDING_OFF);
-								goal.varCheck(validator);
-								goal.typeCheck(validator);
-								assertEqualTypes(goal.children.slice(5).map((stmt) => (stmt as AST.ASTNodeStatementExpression).expr!.type(validator)), [
-									Int16.union(hello),
-									Int16.union(hello),
-									SolidBoolean.TRUETYPE.union(hello),
-									SolidBoolean.TRUETYPE.union(Float64).union(hello),
-									SolidString.union(typeConstInt(42n)),
-								]);
-							});
-							it('returns `left` if it does not contain `void` nor `null` nor `false`.', () => {
-								const goal: AST.ASTNodeGoal = AST.ASTNodeGoal.fromSource(`
-									let unfixed a: int = 42;
-									let unfixed b: float = 4.2;
-									a || true;
-									b || null;
-								`, CONFIG_FOLDING_OFF);
-								const validator: Validator = new Validator(CONFIG_FOLDING_OFF);
-								goal.varCheck(validator);
-								goal.typeCheck(validator);
-								assert.deepStrictEqual(goal.children.slice(2).map((stmt) => (stmt as AST.ASTNodeStatementExpression).expr!.type(validator)), [
-									Int16,
-									Float64,
-								]);
-							});
-						});
-					});
-				});
 				describe('ASTNodeOperationTernary', () => {
 					context('with constant folding on', () => {
 						it('computes type for for conditionals', () => {
@@ -1229,20 +1025,6 @@ describe('ASTNodeSolid', () => {
 						[...tests.values()],
 					);
 				}
-				specify('ASTNodeOperationBinaryLogical', () => {
-					foldOperations(new Map<string, SolidObject>([
-						[`null && 5;`,     SolidNull.NULL],
-						[`null || 5;`,     new Int16(5n)],
-						[`5 && null;`,     SolidNull.NULL],
-						[`5 || null;`,     new Int16(5n)],
-						[`5.1 && true;`,   SolidBoolean.TRUE],
-						[`5.1 || true;`,   new Float64(5.1)],
-						[`3.1 && 5;`,      new Int16(5n)],
-						[`3.1 || 5;`,      new Float64(3.1)],
-						[`false && null;`, SolidBoolean.FALSE],
-						[`false || null;`, SolidNull.NULL],
-					]))
-				})
 				specify('ASTNodeOperationTernary', () => {
 					foldOperations(new Map<string, SolidObject>([
 						[`if true then false else 2;`,          SolidBoolean.FALSE],
