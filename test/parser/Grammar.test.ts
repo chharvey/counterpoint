74bcc08b98c16b7ccbc66811c1f9c6c1cb22607c
Chris Harvey <chrisharvey2pi@gmail.com>
2021-10-28 22:18:45 -0400

    fixup 1ca76618

--- test/parser/Grammar.test.ts
+++ test/parser/Grammar.test.ts
@@ -7,24 +7,22 @@ import {Grammar} from '../../src/parser/Grammar.js';
 describe('Grammar', () => {
 	describe('.fromJSON', () => {
 		it('returns a string representing a new instance of Grammar.', () => {
-			assert.strictEqual(Grammar.fromJSON(JSON.parse(`
-				[
-					{
-						"name": "Unit",
-						"defn": [
-							[{"term":"NUMBER"}],
-							["(", {"term":"OPERATOR"}, {"prod":"Unit"}, {"prod":"Unit"}, ")"]
-						]
-					},
-					{
-						"name": "Goal",
-						"defn": [
-							["\\u0002", "\\u0003"],
-							["\\u0002", {"prod":"Unit"}, "\\u0003"]
-						]
-					}
-				]
-			`)), xjs.String.dedent`
+			assert.strictEqual(Grammar.fromJSON([
+				{
+					name: 'Unit',
+					defn: [
+						[{term: 'NUMBER'}],
+						['(', {term: 'OPERATOR'}, {prod: 'Unit'}, {prod: 'Unit'}, ')'],
+					],
+				},
+				{
+					name: 'Goal',
+					defn: [
+						['\\u0002',                 '\\u0003'],
+						['\\u0002', {prod: 'Unit'}, '\\u0003'],
+					],
+				},
+			]), xjs.String.dedent`
 				export const GRAMMAR: Grammar = new Grammar([
 					ProductionUnit.instance,
 					ProductionGoal.instance,
