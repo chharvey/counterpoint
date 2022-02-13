c20389cbbb171e6c939e0e109bcd72f7f872653f
Chris Harvey <chrisharvey2pi@gmail.com>
2022-02-13 14:29:47 -0500

    DELETE `DecoratorEbnf`
    deleted:    src/validator/DecoratorEbnf.ts
    deleted:    tasks/build-parser.js
    deleted:    test/validator/DecoratorEbnf.test.ts
    deleted:    test/validator/astnode-ebnf/ASTNodeEbnf.test.ts
    deleted:    test/validator/astnode-ebnf/ASTNodeExpr.test.ts
    deleted:    test/validator/astnode-ebnf/ASTNodeOp.test.ts

--- src/validator/index.ts
+++ src/validator/index.ts
@@ -5,7 +5,6 @@ export * from './ASTNode.js';
 export * as ASTNODE_EBNF from './astnode-ebnf/index.js';
 export * as ASTNODE_SOLID from './astnode-solid/index.js';
 
-export {DECORATOR as DECORATOR_EBNF} from './DecoratorEbnf.js';
 export {DECORATOR as DECORATOR_SOLID} from './DecoratorSolid.js';
 export * from './Validator.js';
 
