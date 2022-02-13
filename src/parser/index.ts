3ff291b77e773d5689298a49c97ee8fe84cdbbe3
Chris Harvey <chrisharvey2pi@gmail.com>
2022-02-13 16:22:47 -0500

    DELETE `token-ebnf`
    deleted:    src/parser/token-ebnf/TokenCharClass.ts
    deleted:    src/parser/token-ebnf/TokenCharCode.ts
    deleted:    src/parser/token-ebnf/TokenCommentEbnf.ts
    deleted:    src/parser/token-ebnf/TokenIdentifier.ts
    deleted:    src/parser/token-ebnf/TokenPunctuator.ts
    deleted:    src/parser/token-ebnf/TokenString.ts
    deleted:    src/parser/token-ebnf/index.ts
    deleted:    src/parser/token-ebnf/package.ts

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -5,7 +5,6 @@ export * from './Keyword.js';
 
 export * from './Char.js';
 export * from './Token.js';
-export * as TOKEN_EBNF from './token-ebnf/index.js';
 export * as TOKEN_SOLID from './token-solid/index.js';
 export * from './ParseNode.js';
 
