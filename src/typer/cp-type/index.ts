bbe093086888078ba9041cf173e8e7e7725b1518
Chris Harvey <1362083+chharvey@users.noreply.github.com>
2022-10-29 12:04:21 -0400

    move Type{Never,Void,Unknown} into own files

--- src/typer/cp-type/index.ts
+++ src/typer/cp-type/index.ts
@@ -5,11 +5,14 @@ export * from './TypeUnion.js';
 export * from './TypeDifference.js';
 export * from './TypeUnit.js';
 
+export * from './TypeNever.js';
+export * from './TypeVoid.js';
+export * from './TypeUnknown.js';
+export * from './TypeObject.js';
 export * from './TypeBoolean.js';
 export * from './TypeInteger.js';
 export * from './TypeFloat.js';
 export * from './TypeString.js';
-export * from './TypeObject.js';
 
 export * from './TypeTuple.js';
 export * from './TypeRecord.js';
