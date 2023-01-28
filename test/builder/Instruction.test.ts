b540da20873b0e793f38debc5f2b824b38f080b7
Chris Harvey <1362083+chharvey@users.noreply.github.com>
2023-01-28 12:37:41 -0500

    add buildBin tests

--- test/builder/Instruction.test.ts
+++ test/builder/Instruction.test.ts
@@ -3,7 +3,9 @@ import binaryen from 'binaryen';
 import {
 	Operator,
 	INST,
+	Builder,
 } from '../../src/index.js';
+import {assertEqualBins} from '../assert-helpers.js';
 import {
 	instructionConstInt,
 	instructionConstFloat,
@@ -55,7 +57,7 @@ describe('Instruction', () => {
 
 
 	describe('InstructionConst', () => {
-		describe('#toString', () => {
+		describe('#{toString,buildBin}', () => {
 			it('pushes the constant integer onto the stack.', () => {
 				const values: number[] = [
 					0,
@@ -78,6 +80,11 @@ describe('Instruction', () => {
 					values.map((x) => instructionConstInt(BigInt(x)).toString()),
 					values.map((x) => `(i32.const ${ x })`),
 				)
+				const mod: binaryen.Module = new Builder('').module;
+				return assertEqualBins(
+					values.map((x) => instructionConstInt(BigInt(x)).buildBin(mod)),
+					values.map((x) => mod.i32.const(x)),
+				);
 			})
 			it('pushes the constant float onto the stack.', () => {
 				const values: number[] = [
@@ -88,16 +95,30 @@ describe('Instruction', () => {
 					values.map((x) => instructionConstFloat(x).toString()),
 					values.map((x) => `(f64.const ${ x }${ (x % 1 === 0) ? '.0' : '' })`),
 				)
+				const mod: binaryen.Module = new Builder('').module;
+				return assertEqualBins(
+					values.map((x) => instructionConstFloat(x).buildBin(mod)),
+					values.map((x) => mod.f64.const(x)),
+				);
 			})
 			it('prints Float64 negative zero correctly.', () => {
 				assert.strictEqual(instructionConstFloat(-0.0).toString(), `(f64.const -0.0)`)
+				const mod: binaryen.Module = new Builder('').module;
+				assert.throws(() => assertEqualBins(
+					instructionConstFloat(-0.0).buildBin(mod),
+					mod.f64.const(-0.0),
+				));
+				return assertEqualBins(
+					instructionConstFloat(-0.0).buildBin(mod),
+					mod.f64.ceil(mod.f64.const(-0.5)),
+				);
 			})
 		})
 	});
 
 
 	describe('InstructionConvert', () => {
-		describe('#toString', () => {
+		describe('#{toString,buildBin}', () => {
 			it('converts an i32 into an f64.', () => {
 				const exprs = [
 					instructionConstInt(3n),
@@ -111,13 +132,18 @@ describe('Instruction', () => {
 					exprs.map((expr) => new INST.InstructionConvert(expr).toString()),
 					exprs.map((expr) => `(f64.convert_i32_u ${ expr })`),
 				);
+				const mod: binaryen.Module = new Builder('').module;
+				return assertEqualBins(
+					exprs.map((expr) => new INST.InstructionConvert(expr).buildBin(mod)),
+					exprs.map((expr) => mod.f64.convert_u.i32(expr.buildBin(mod))),
+				);
 			});
 		});
 	});
 
 
 	describe('InstructionUnop', () => {
-		describe('#toString', () => {
+		describe('#{toString,buildBin}', () => {
 			it('performs a unary operation.', () => {
 				assert.deepStrictEqual([
 					new INST.InstructionUnop(Operator.NOT, instructionConstInt(0n)),
@@ -140,104 +166,129 @@ describe('Instruction', () => {
 					`(call $femp ${ instructionConstFloat(4.2) })`,
 					`(call $neg ${ instructionConstInt(42n) })`,
 				])
+				const mod: binaryen.Module = new Builder('').module;
+				return assertEqualBins([
+					new INST.InstructionUnop(Operator.NOT, instructionConstInt(0n)),
+					new INST.InstructionUnop(Operator.NOT, instructionConstInt(42n)),
+					new INST.InstructionUnop(Operator.NOT, instructionConstFloat(0.0)),
+					new INST.InstructionUnop(Operator.NOT, instructionConstFloat(4.2)),
+					new INST.InstructionUnop(Operator.EMP, instructionConstInt(0n)),
+					new INST.InstructionUnop(Operator.EMP, instructionConstInt(42n)),
+					new INST.InstructionUnop(Operator.EMP, instructionConstFloat(0.0)),
+					new INST.InstructionUnop(Operator.EMP, instructionConstFloat(4.2)),
+					new INST.InstructionUnop(Operator.NEG, instructionConstInt(42n)),
+				].map((inst) => inst.buildBin(mod)), [
+					mod.call('inot', [instructionConstInt(0n)    .buildBin(mod)], binaryen.i32),
+					mod.call('inot', [instructionConstInt(42n)   .buildBin(mod)], binaryen.i32),
+					mod.call('fnot', [instructionConstFloat(0.0) .buildBin(mod)], binaryen.i32),
+					mod.call('fnot', [instructionConstFloat(4.2) .buildBin(mod)], binaryen.i32),
+					mod.call('iemp', [instructionConstInt(0n)    .buildBin(mod)], binaryen.i32),
+					mod.call('iemp', [instructionConstInt(42n)   .buildBin(mod)], binaryen.i32),
+					mod.call('femp', [instructionConstFloat(0.0) .buildBin(mod)], binaryen.i32),
+					mod.call('femp', [instructionConstFloat(4.2) .buildBin(mod)], binaryen.i32),
+					mod.call('neg',  [instructionConstInt(42n)   .buildBin(mod)], binaryen.i32),
+				]);
 			})
 		})
 	});
 
 
 	describe('InstructionBinop', () => {
-		describe('#toString', () => {
+		describe('#{toString,buildBin}', () => {
 			it('performs a binary operation.', () => {
-				assert.strictEqual(new INST.InstructionBinopArithmetic(
-					Operator.MUL,
-					instructionConstInt(21n),
-					instructionConstInt(2n),
-				).toString(), `(i32.mul ${ instructionConstInt(21n) } ${ instructionConstInt(2n) })`)
-				assert.strictEqual(new INST.InstructionBinopArithmetic(
-					Operator.ADD,
-					instructionConstFloat(30.1),
-					instructionConstFloat(18.1),
-				).toString(), `(f64.add ${ instructionConstFloat(30.1) } ${ instructionConstFloat(18.1) })`)
-				assert.strictEqual(new INST.InstructionBinopComparative(
-					Operator.LT,
-					instructionConstInt(30n),
-					instructionConstInt(18n),
-				).toString(), `(i32.lt_s ${ instructionConstInt(30n) } ${ instructionConstInt(18n) })`)
-				assert.strictEqual(new INST.InstructionBinopComparative(
-					Operator.GE,
-					instructionConstFloat(30.1),
-					instructionConstFloat(18.1),
-				).toString(), `(f64.ge ${ instructionConstFloat(30.1) } ${ instructionConstFloat(18.1) })`)
-				assert.strictEqual(new INST.InstructionBinopEquality(
-					Operator.ID,
-					instructionConstInt(30n),
-					instructionConstInt(18n),
-				).toString(), `(i32.eq ${ instructionConstInt(30n) } ${ instructionConstInt(18n) })`)
-				assert.strictEqual(new INST.InstructionBinopEquality(
-					Operator.ID,
-					instructionConstFloat(30.1),
-					instructionConstFloat(18.1),
-				).toString(), `(call $fid ${ instructionConstFloat(30.1) } ${ instructionConstFloat(18.1) })`)
-				assert.strictEqual(new INST.InstructionBinopEquality(
-					Operator.ID,
-					instructionConstInt(30n),
-					instructionConstFloat(18.1),
-				).toString(), `(call $i_f_id ${ instructionConstInt(30n) } ${ instructionConstFloat(18.1) })`)
-				assert.strictEqual(new INST.InstructionBinopEquality(
-					Operator.ID,
-					instructionConstFloat(18.1),
-					instructionConstInt(30n),
-				).toString(), `(call $f_i_id ${ instructionConstFloat(18.1) } ${ instructionConstInt(30n) })`)
-				assert.strictEqual(new INST.InstructionBinopEquality(
-					Operator.EQ,
-					instructionConstInt(30n),
-					instructionConstFloat(18.1),
-				).toString(), `(call $i_f_id ${ instructionConstInt(30n) } ${ instructionConstFloat(18.1) })`)
-				assert.strictEqual(new INST.InstructionBinopEquality(
-					Operator.EQ,
-					instructionConstFloat(18.1),
-					instructionConstInt(30n),
-				).toString(), `(call $f_i_id ${ instructionConstFloat(18.1) } ${ instructionConstInt(30n) })`)
+				const actuals: readonly INST.Instruction[] = [
+					new INST.InstructionBinopArithmetic(Operator.MUL, instructionConstInt(21n),    instructionConstInt(2n)),
+					new INST.InstructionBinopArithmetic(Operator.ADD, instructionConstFloat(30.1), instructionConstFloat(18.1)),
+					new INST.InstructionBinopComparative(Operator.LT, instructionConstInt(30n),    instructionConstInt(18n)),
+					new INST.InstructionBinopComparative(Operator.GE, instructionConstFloat(30.1), instructionConstFloat(18.1)),
+					new INST.InstructionBinopEquality(Operator.ID, instructionConstInt(30n),    instructionConstInt(18n)),
+					new INST.InstructionBinopEquality(Operator.ID, instructionConstFloat(30.1), instructionConstFloat(18.1)),
+					new INST.InstructionBinopEquality(Operator.ID, instructionConstInt(30n),    instructionConstFloat(18.1)),
+					new INST.InstructionBinopEquality(Operator.ID, instructionConstFloat(18.1), instructionConstInt(30n)),
+					new INST.InstructionBinopEquality(Operator.EQ, instructionConstInt(30n),    instructionConstFloat(18.1)),
+					new INST.InstructionBinopEquality(Operator.EQ, instructionConstFloat(18.1), instructionConstInt(30n)),
+				];
+				assert.deepStrictEqual(actuals.map((actual) => actual.toString()), [
+					`(i32.mul ${      instructionConstInt(21n)    } ${ instructionConstInt(2n) })`,
+					`(f64.add ${      instructionConstFloat(30.1) } ${ instructionConstFloat(18.1) })`,
+					`(i32.lt_s ${     instructionConstInt(30n)    } ${ instructionConstInt(18n) })`,
+					`(f64.ge ${       instructionConstFloat(30.1) } ${ instructionConstFloat(18.1) })`,
+					`(i32.eq ${       instructionConstInt(30n)    } ${ instructionConstInt(18n) })`,
+					`(call $fid ${    instructionConstFloat(30.1) } ${ instructionConstFloat(18.1) })`,
+					`(call $i_f_id ${ instructionConstInt(30n)    } ${ instructionConstFloat(18.1) })`,
+					`(call $f_i_id ${ instructionConstFloat(18.1) } ${ instructionConstInt(30n) })`,
+					`(call $i_f_id ${ instructionConstInt(30n)    } ${ instructionConstFloat(18.1) })`,
+					`(call $f_i_id ${ instructionConstFloat(18.1) } ${ instructionConstInt(30n) })`,
+				]);
+				const mod: binaryen.Module = new Builder('').module;
+				return assertEqualBins(actuals.map((actual) => actual.buildBin(mod)), [
+					mod.i32.mul  (instructionConstInt(21n)    .buildBin(mod), instructionConstInt(2n)     .buildBin(mod)),
+					mod.f64.add  (instructionConstFloat(30.1) .buildBin(mod), instructionConstFloat(18.1) .buildBin(mod)),
+					mod.i32.lt_s (instructionConstInt(30n)    .buildBin(mod), instructionConstInt(18n)    .buildBin(mod)),
+					mod.f64.ge   (instructionConstFloat(30.1) .buildBin(mod), instructionConstFloat(18.1) .buildBin(mod)),
+					mod.i32.eq   (instructionConstInt(30n)    .buildBin(mod), instructionConstInt(18n)    .buildBin(mod)),
+					mod.call('fid',    [instructionConstFloat(30.1) .buildBin(mod), instructionConstFloat(18.1) .buildBin(mod)], binaryen.i32),
+					mod.call('i_f_id', [instructionConstInt(30n)    .buildBin(mod), instructionConstFloat(18.1) .buildBin(mod)], binaryen.i32),
+					mod.call('f_i_id', [instructionConstFloat(18.1) .buildBin(mod), instructionConstInt(30n)    .buildBin(mod)], binaryen.i32),
+					mod.call('i_f_id', [instructionConstInt(30n)    .buildBin(mod), instructionConstFloat(18.1) .buildBin(mod)], binaryen.i32),
+					mod.call('f_i_id', [instructionConstFloat(18.1) .buildBin(mod), instructionConstInt(30n)    .buildBin(mod)], binaryen.i32),
+				]);
 			});
 			it('creates variables for logical operations.', () => {
-				assert.strictEqual(new INST.InstructionBinopLogical(
-					0,
-					Operator.AND,
-					instructionConstInt(30n),
-					instructionConstInt(18n),
-				).toString(), new INST.InstructionCond(
-					new INST.InstructionUnop(Operator.NOT, new INST.InstructionUnop(Operator.NOT, new INST.InstructionLocalTee(0, instructionConstInt(30n)))),
-					instructionConstInt(18n),
-					new INST.InstructionLocalGet(0, binaryen.i32),
-				).toString());
-				assert.strictEqual(new INST.InstructionBinopLogical(
-					3,
-					Operator.OR,
-					instructionConstFloat(30.1),
-					instructionConstFloat(18.1),
-				).toString(), new INST.InstructionCond(
-					new INST.InstructionUnop(Operator.NOT, new INST.InstructionUnop(Operator.NOT, new INST.InstructionLocalTee(3, instructionConstFloat(30.1)))),
-					new INST.InstructionLocalGet(3, binaryen.f64),
-					instructionConstFloat(18.1),
-				).toString());
+				const actuals: readonly INST.InstructionBinopLogical[] = [
+					new INST.InstructionBinopLogical(0, Operator.AND, instructionConstInt(30n),    instructionConstInt(18n)),
+					new INST.InstructionBinopLogical(3, Operator.OR,  instructionConstFloat(30.1), instructionConstFloat(18.1)),
+				];
+				const expecteds: readonly INST.InstructionCond[] = [
+					new INST.InstructionCond(
+						new INST.InstructionUnop(Operator.NOT, new INST.InstructionUnop(Operator.NOT, new INST.InstructionLocalTee(0, instructionConstInt(30n)))),
+						instructionConstInt(18n),
+						new INST.InstructionLocalGet(0, binaryen.i32),
+					),
+					new INST.InstructionCond(
+						new INST.InstructionUnop(Operator.NOT, new INST.InstructionUnop(Operator.NOT, new INST.InstructionLocalTee(3, instructionConstFloat(30.1)))),
+						new INST.InstructionLocalGet(3, binaryen.f64),
+						instructionConstFloat(18.1),
+					),
+				];
+				assert.deepStrictEqual(
+					actuals   .map((actual) =>   actual.toString()),
+					expecteds .map((expected) => expected.toString()),
+				);
+				const mod: binaryen.Module = new Builder('').module;
+				return assertEqualBins(
+					actuals   .map((actual) =>   actual.buildBin(mod)),
+					expecteds .map((expected) => expected.buildBin(mod)),
+				);
 			})
 		})
 	});
 
 
 	describe('InstructionCond', () => {
-		describe('#toString', () => {
+		describe('#{toString, buildBin}', () => {
 			it('performs a conditional operation.', () => {
-				assert.strictEqual(new INST.InstructionCond(
-					instructionConstInt(1n),
-					instructionConstInt(2n),
-					instructionConstInt(3n),
-				).toString(), `(if (result i32) ${ instructionConstInt(1n) } (then ${ instructionConstInt(2n) }) (else ${ instructionConstInt(3n) }))`)
-				assert.strictEqual(new INST.InstructionCond(
-					instructionConstInt(0n),
-					instructionConstFloat(2.2),
-					instructionConstFloat(3.3),
-				).toString(), `(if (result f64) ${ instructionConstInt(0n) } (then ${ instructionConstFloat(2.2) }) (else ${ instructionConstFloat(3.3) }))`)
+				const actuals: readonly INST.InstructionCond[] = [
+					new INST.InstructionCond(
+						instructionConstInt(1n),
+						instructionConstInt(2n),
+						instructionConstInt(3n),
+					),
+					new INST.InstructionCond(
+						instructionConstInt(0n),
+						instructionConstFloat(2.2),
+						instructionConstFloat(3.3),
+					),
+				];
+				assert.deepStrictEqual(actuals.map((actual) => actual.toString()), [
+					`(if (result i32) ${ instructionConstInt(1n) } (then ${ instructionConstInt(2n)    }) (else ${ instructionConstInt(3n) }))`,
+					`(if (result f64) ${ instructionConstInt(0n) } (then ${ instructionConstFloat(2.2) }) (else ${ instructionConstFloat(3.3) }))`,
+				]);
+				const mod: binaryen.Module = new Builder('').module;
+				return assertEqualBins(actuals.map((actual) => actual.buildBin(mod)), [
+					mod.if(instructionConstInt(1n).buildBin(mod), instructionConstInt(2n)    .buildBin(mod), instructionConstInt(3n)    .buildBin(mod)),
+					mod.if(instructionConstInt(0n).buildBin(mod), instructionConstFloat(2.2) .buildBin(mod), instructionConstFloat(3.3) .buildBin(mod)),
+				]);
 			})
 		})
 	})
