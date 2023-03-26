7f3b76989688469d83af547fcbfaaab2e73870d2
Chris Harvey <1362083+chharvey@users.noreply.github.com>
2023-03-26 19:54:35 -0400

    clean `isBottomType` initializers

--- src/typer/cp-type/TypeDifference.ts
+++ src/typer/cp-type/TypeDifference.ts
@@ -11,7 +11,12 @@ import {Type} from './Type.js';
  * that contains values assignable to `T` but *not* assignable to `U`.
  */
 export class TypeDifference extends Type {
-	public declare readonly isBottomType: boolean;
+	/* We can assert that this is always non-empty because
+	the only cases in which it could be empty are
+	1. if left is empty
+	2. if left is a subtype of right
+	each of which is impossible because the algorithm would have already produced the `never` type. */
+	public override readonly isBottomType: boolean = false;
 
 	/**
 	 * Construct a new TypeDifference object.
@@ -23,14 +28,6 @@ export class TypeDifference extends Type {
 		private readonly right: Type,
 	) {
 		super(false, xjs.Set.difference(left.values, right.values, languageValuesIdentical));
-		/*
-		We can assert that this is always non-empty because
-		the only cases in which it could be empty are
-		1. if left is empty
-		2. if left is a subtype of right
-		each of which is impossible because the algorithm would have already produced the `never` type.
-		*/
-		this.isBottomType = false;
 	}
 
 	public override get hasMutable(): boolean {
