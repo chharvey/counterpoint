1930688738627ed837482785b213d4b1dddb618d
Chris Harvey <1362083+chharvey@users.noreply.github.com>
2024-04-11 18:17:39 -0400

    perf: use WeakMaps as lookup tables

--- src/typer/cp-object/Object.ts
+++ src/typer/cp-object/Object.ts
@@ -1,9 +1,5 @@
 import * as assert from 'assert';
-import * as xjs from 'extrajs';
-import {
-	type Keys,
-	strictEqual,
-} from '../../lib/index.js';
+import {strictEqual} from '../../lib/index.js';
 import type {TYPE} from '../index.js';
 import {String as CPString} from './index.js';
 
@@ -17,9 +13,9 @@ import {String as CPString} from './index.js';
  */
 abstract class CPObject {
 	/** Memoizer for comparing `CPObject`s by identity (`===`). */
-	private static readonly ID_MEMO = new Map<readonly [CPObject, CPObject], boolean>();
+	private static readonly ID_MEMO = new WeakMap<CPObject, WeakMap<CPObject, boolean>>();
 	/** Memoizer for comparing `CPObject`s by equality (`==`). */
-	private static readonly EQ_MEMO = new Map<readonly [CPObject, CPObject], boolean>();
+	private static readonly EQ_MEMO = new WeakMap<CPObject, WeakMap<CPObject, boolean>>();
 
 	/**
 	 * Decorator for {@link CPObject#identical} or {@link CPObject#equal} for memoizing results.
@@ -30,21 +26,34 @@ abstract class CPObject {
 		method:  CPObject['identical' | 'equal'],
 		context: ClassMethodDecoratorContext<CPObject, typeof method>,
 	): typeof method {
-		const memo: Map<readonly [CPObject, CPObject], boolean> = (
+		const memo: WeakMap<CPObject, WeakMap<CPObject, boolean>> = (
 			context.name === 'identical' ? CPObject.ID_MEMO :
 			context.name === 'equal'     ? CPObject.EQ_MEMO :
 			assert.fail(`CPObject.memoizeSameness did not expect the name \`${ context.name.toString() }\`.`)
 		);
-		type K = Keys<typeof memo>;
-		const memo_comparator = (key0: K, key1: K): boolean => xjs.Set.is<CPObject>(new Set<CPObject>(key0), new Set<CPObject>(key1));
-
 		return function (this: CPObject, value) {
-			const memo_key: K = [this, value];
-			if (!xjs.Map.has(memo, memo_key, memo_comparator)) {
-				xjs.Map.set(memo, memo_key, true,                     memo_comparator); // use this assumption in the next step
-				xjs.Map.set(memo, memo_key, method.call(this, value), memo_comparator);
+			if (memo.has(this)) {
+				const map: WeakMap<CPObject, boolean> = memo.get(this)!;
+				if (!map.has(value)) {
+					map.set(value, true); // use this assumption in the next step
+					map.set(value, method.call(this, value));
+				}
+				return map.get(value)!;
+			} else if (memo.has(value)) {
+				const map: WeakMap<CPObject, boolean> = memo.get(value)!;
+				if (!map.has(this)) {
+					map.set(this, true); // use this assumption in the next step
+					map.set(this, method.call(this, value));
+				}
+				return map.get(this)!;
+			} else {
+				const map = new WeakMap<CPObject, boolean>();
+				memo.set(this, map);
+				map.set(value, true); // use this assumption in the next step
+				const result: boolean = method.call(this, value);
+				map.set(value, result);
+				return result;
 			}
-			return xjs.Map.get(memo, memo_key, memo_comparator)!;
 		};
 	}
 
