e1700af71c68f587ba985f37d961e5a87ea4ffe0
Chris Harvey <1362083+chharvey@users.noreply.github.com>
2025-02-18 23:55:51 -0500

    refactor: use TS constructor params

--- src/builder/Local.ts
+++ src/builder/Local.ts
@@ -6,12 +6,6 @@ import binaryen from 'binaryen';
  * A class modeling the Binaryen `module.local` instructions.
  */
 export class Local {
-	/** A Binaryen module to send instructions to. */
-	readonly #module: binaryen.Module;
-
-	/** The variable’s WASM index. */
-	readonly #index: number;
-
 	/* The Binaryen value of the variable. */
 	#value: binaryen.ExpressionRef;
 
@@ -20,21 +14,19 @@ export class Local {
 
 	/**
 	 * Construct a new Local object.
-	 * @param module a Binaryen module to send instructions to
+	 * @param module A Binaryen module to send instructions to.
 	 * @param id     The compiler’s internal identifier for the variable.
-	 * @param index  a WASM variable index
+	 * @param index  The variable’s WASM index.
 	 * @param value  The Binaryen value of the variable.
 	 */
 	public constructor(
-		module: binaryen.Module,
-		public readonly id: bigint,
-		index: number,
+		private readonly module: binaryen.Module,
+		public  readonly id:     bigint,
+		private readonly index:  number,
 		value: binaryen.ExpressionRef,
 	) {
-		this.#module = module;
-		this.#index  = index;
-		this.#value  = value;
-		this.type    = binaryen.getExpressionType(value);
+		this.#value = value;
+		this.type   = binaryen.getExpressionType(value);
 	}
 
 	/* The Binaryen value of the variable. */
@@ -46,17 +38,17 @@ export class Local {
 		if (value !== undefined) {
 			this.#value = value;
 		}
-		return this.#module.local.set(this.#index, this.#value);
+		return this.module.local.set(this.index, this.#value);
 	}
 
 	public get(): binaryen.ExpressionRef {
-		return this.#module.local.get(this.#index, this.type);
+		return this.module.local.get(this.index, this.type);
 	}
 
 	public tee(value?: binaryen.ExpressionRef): binaryen.ExpressionRef {
 		if (value !== undefined) {
 			this.#value = value;
 		}
-		return this.#module.local.tee(this.#index, this.#value, this.type);
+		return this.module.local.tee(this.index, this.#value, this.type);
 	}
 }
