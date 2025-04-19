dc91a2561fb773c22eed25f8eed0ab3857de768b
Chris Harvey <1362083+chharvey@users.noreply.github.com>
2025-04-19 10:05:24 -0400

    refactor: move `forEither` to utils

--- src/lib/utils-public.ts
+++ src/lib/utils-public.ts
@@ -60,3 +60,43 @@ export function assert_instanceof<Class extends object>(obj: unknown, cons: Subc
 export function assert_context_name(context: ClassMethodDecoratorContext, name: string): void {
 	return assert.strictEqual(context.name, name, `This decorator may only be used on methods named \`${ name }\`.`);
 }
+
+
+
+/**
+ * Executes a callback on each item of an array until that callback returns.
+ * If any iteration throws, the error is saved, and execution proceeds to the next iteration.
+ * If any iteration returns, this method returns and the errors are discarded.
+ * If all iterations throw, the errors are collected into a single AggregateError, which is then thrown.
+ *
+ * The “dual” of {@link Array#forEach} — this method returns as soon as a callback call is successful; otherwise throws.
+ *
+ * Similar to {@link Promise.any}, but synchronous.
+ *
+ * @typeparam T                the type of items in the array
+ * @param     array            the array of items
+ * @param     callback         the function to call on each item
+ * @throws    {AggregateError} if two or more iterations throws an error
+ * @throws    {Error}          if one iteration throws an error
+ */
+export function forEither<T>(array: readonly T[], callback: (item: T, i: number, src: typeof array) => void): void {
+	const thrown: Error[] = [];
+	try {
+		array.forEach((it, i, src) => {
+			try {
+				callback.call(null, it, i, src);
+			} catch (e) {
+				thrown.push(e as Error);
+				return;
+			}
+			throw new Error('success');
+		});
+	} catch {
+		return;
+	}
+	throw (
+		thrown.length >= 2 ? new AggregateError(thrown) :
+		thrown.length      ? thrown[0] :
+		new Error('An unexpected error occurred.')
+	);
+}
