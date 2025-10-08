c3d21bd56fd6455315a0051c360fb4f1a5c2eb28
Chris Harvey <1362083+chharvey@users.noreply.github.com>
2025-10-07 20:51:29 -0400

    feat: short-circuit multiplication when left operand is zero

--- test/validator/astnode-cp/ASTNodeOperation.test.ts
+++ test/validator/astnode-cp/ASTNodeOperation.test.ts
@@ -715,6 +715,33 @@ describe('ASTNodeOperation', () => {
 					['3.0 * 2.1;',        new VALUE.Float(3.0 * 2.1)],
 				]));
 			});
+			it('short-circuits when multiplicand is zero.', () => {
+				const goal: AST.ASTNodeGoal = AST.ASTNodeGoal.fromSource(`
+					let var i: int   = 42;
+					let var f: float = 4.2;
+
+					0 * i;    % value \`0\`
+					0.0 * f;  % value \`0.0\`
+					-0.0 * f; % value \`-0.0\`
+
+					1 * i;    % non-foldable value
+					1.0 * f;  % non-foldable value
+					-1.0 * f; % non-foldable value
+				`);
+				goal.varCheck();
+				goal.typeCheck();
+				goal.build();
+				const exprs:     readonly AST.ASTNodeExpression[] = goal.children.slice(2).map((stmt) => ((stmt as AST.ASTNodeStatementExpression).expr!));
+				const expecteds: readonly (VALUE.Value | null)[]  = exprs.slice(0, 3).map((op) => (op as AST.ASTNodeOperationBinaryArithmetic).operand0.fold());
+				assert.deepStrictEqual(
+					exprs.map((op) => op.fold()),
+					[...expecteds, null, null, null],
+				);
+				return assert.deepStrictEqual(
+					expecteds,
+					[VALUE.INT_0, VALUE.FLOAT_0, VALUE.FLOAT_N0],
+				);
+			});
 			it('throws when performing an operation that does not yield a valid number.', () => {
 				assert.throws(() => AST.ASTNodeOperationBinaryArithmetic.fromSource('42 / 0;')     .fold(), NanErrorDivZero);
 				assert.throws(() => AST.ASTNodeOperationBinaryArithmetic.fromSource('-4.0 ^ -0.5;').fold(), NanErrorInvalid);
