6273b29543b27327813cc360b7e033a4b9ebd44e
Chris Harvey <1362083+chharvey@users.noreply.github.com>
2025-10-11 23:35:04 -0400

    fixup! a7f794d1

--- src/validator/astnode-cp/ASTNodeClaim.ts
+++ src/validator/astnode-cp/ASTNodeClaim.ts
@@ -47,7 +47,7 @@ export class ASTNodeClaim extends ASTNodeExpression {
 		const computed_type: TYPE.Type = this.operand.type();
 		const claimed_type:  TYPE.Type = this.claimed_type.eval();
 		/* If the types are disjoint and neither of the types are the Bottom Type, throw an error. */
-		if (claimed_type.intersect(computed_type).isBottomType && !computed_type.isBottomType && !claimed_type.isBottomType) {
+		if (computed_type.intersect(claimed_type).isBottomType && !computed_type.isBottomType && !claimed_type.isBottomType) {
 			/*
 				`Conversion of type \`${ computed_type }\` to type \`${ claimed_type }\` may be a mistake
 				because neither type sufficiently overlaps with the other.
