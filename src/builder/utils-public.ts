b0ba893811433d7d412733481d59a58048406845
Chris Harvey <1362083+chharvey@users.noreply.github.com>
2026-05-17 01:06:16 -0400

    refactor: update `drop_then` param

--- src/builder/utils-public.ts
+++ src/builder/utils-public.ts
@@ -1,5 +1,5 @@
 import binaryen from 'binaryen';
-import type {Builder} from './index.ts';
+import type {VirtualMachine} from '../vm/index.ts';
 
 
 
@@ -36,15 +36,15 @@ export function bigint_to_i64(mod: binaryen.Module, value: bigint, u: boolean = 
  * Return a block containing `(drop)` expressions for each of `args`, followed by a final expression.
  * If `final` is provided as an ExpressionRef, it is the final expression;
  * otherwise, a v128 containing a boolean encoding is the final expression.
- * @param cg
+ * @param vm
  * @param args  the args to drop first
  * @param final the final expression/statement
  */
 export function drop_then(
-	cg:    Builder,
+	vm:    VirtualMachine,
 	args:  readonly binaryen.ExpressionRef[],
 	final: binaryen.ExpressionRef | boolean,
 ): binaryen.ExpressionRef {
-	const last_item: binaryen.ExpressionRef = typeof final === 'number' ? final : final ? cg.vm.Vect.TRUE : cg.vm.Vect.FALSE;
-	return cg.vm.mod.block(null, [...args.map((arg) => cg.vm.mod.drop(arg)), last_item], binaryen.getExpressionType(last_item));
+	const last_item: binaryen.ExpressionRef = typeof final === 'number' ? final : final ? vm.Vect.TRUE : vm.Vect.FALSE;
+	return vm.mod.block(null, [...args.map((arg) => vm.mod.drop(arg)), last_item], binaryen.getExpressionType(last_item));
 }
