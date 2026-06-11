b12dfef83a32acbd5258ca654e2c62c89d7e0885
Chris Harvey <1362083+chharvey@users.noreply.github.com>
2026-06-10 23:28:58 -0400

    build(tree-sitter-counterpoint): 3.7.1

--- tree-sitter-counterpoint/src/parser.c
+++ tree-sitter-counterpoint/src/parser.c
@@ -54269,7 +54269,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_counterpoint(void) {
     .metadata = {
       .major_version = 3,
       .minor_version = 7,
-      .patch_version = 0,
+      .patch_version = 1,
     },
   };
   return &language;
