6e919e2e27056e0ac6c3418da261c6970e51610a
Chris Harvey <1362083+chharvey@users.noreply.github.com>
2026-06-13 22:24:41 -0400

    build(tree-sitter-counterpoint): 4.0.0

--- tree-sitter-counterpoint/src/parser.c
+++ tree-sitter-counterpoint/src/parser.c
@@ -53518,8 +53518,8 @@ TS_PUBLIC const TSLanguage *tree_sitter_counterpoint(void) {
     .reserved_words = &ts_reserved_words[0][0],
     .max_reserved_word_set_size = 32,
     .metadata = {
-      .major_version = 3,
-      .minor_version = 8,
+      .major_version = 4,
+      .minor_version = 0,
       .patch_version = 0,
     },
   };
